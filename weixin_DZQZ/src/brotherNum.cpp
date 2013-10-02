094991b8554c8afc88fb270d885b060111e27652
teaspring <bruce.letter@gmail.com>
2013-10-02 17:11:46 +0800

    Update brotherNum.cpp
    reshape backspace

--- weixin_DZQZ/src/brotherNum.cpp
+++ weixin_DZQZ/src/brotherNum.cpp
@@ -18,46 +18,46 @@ using namespace std;
  * 3. iterate the reverted low digit range, find the least digit greater than high digit, swap it with high digit.
  * */
 int reversedigit(int x){
-	int r = 0;
-	int y = x;
-	while(y != 0){
-		r *= 10;
-		r += y % 10;
-		y /= 10;
-	}
-	return r;
+    int r = 0;
+    int y = x;
+    while(y != 0){
+        r *= 10;
+	r += y % 10;
+	y /= 10;
+    }
+    return r;
 }
 
 int getBrotherNum(int x){
-	int y = x;
-	int b = 10;
-	int h = (y/b)%10;
-	int l = y%10;
-	while(h != 0 && h >= l){
-		l = h;
-		b *= 10;
-		h = (y/b)%10;
-	}
-	if(h==0)	return 0;
-	int rev = y % b;
-	y = y  - rev + reversedigit(rev);			//reverse the low range
+    int y = x;
+    int b = 10;
+    int h = (y/b)%10;
+    int l = y%10;
+    while(h != 0 && h >= l){
+        l = h;
+        b *= 10;
+        h = (y/b)%10;
+    }
+    if(h==0)	return 0;
+    int rev = y % b;
+    y = y  - rev + reversedigit(rev);			//reverse the low range
 	
     int order = 1;
-	l = y % 10;
-	while(order <= b && h < l){					//iterate the low range to find the least digit just greater than high digit
-		order *= 10;
-		l = (y/order)%10;
-	}
-	order /= 10;					//get the digit just lower than l
-	int s = (y / order) % 10;
-	return y - h * b - s * order + h * order + s * b;	//swap digit h and s
+    l = y % 10;
+    while(order <= b && h < l){					//iterate the low range to find the least digit just greater than high digit
+        order *= 10;
+	l = (y/order)%10;
+    }
+    order /= 10;					//get the digit just lower than l
+    int s = (y / order) % 10;
+    return y - h * b - s * order + h * order + s * b;	//swap digit h and s
 }
 
 int main(int argc, char* argv[]){
-	int x=0;
-	while(scanf("%d", &x) != EOF){
-		int y = getBrotherNum(x);
-		printf("brother number is %d\n", y);
-	}
-	return 0;
+    int x=0;
+    while(scanf("%d", &x) != EOF){
+    	int y = getBrotherNum(x);
+    	printf("brother number is %d\n", y);
+    }
+    return 0;
 }
