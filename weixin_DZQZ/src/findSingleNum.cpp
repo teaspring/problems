7eceb3cb68a1db40b985d9bfbef97b2947251218
teaspring <bruce.letter@gmail.com>
2013-10-02 18:00:07 +0800

    Update findSingleNum.cpp
    reshape backspace

--- weixin_DZQZ/src/findSingleNum.cpp
+++ weixin_DZQZ/src/findSingleNum.cpp
@@ -10,30 +10,30 @@
 #include "../header/preliminary.h"
 
 int findSingleNum01(int* arr, int n){		// use an integer array of size 32
-	int x=0;
-	for(int i=0;i<32;++i){					//time O(32n) = O(n)
-		int count = 0;
-		int offset = 1<<i;
-		for(int j=0;j<n;++j){
-			count += (arr[j] & offset) >>i;
-		}
-		if(count % 3 == 1)
-		  x |= 1<<i;
-	}
-	return x;
+    int x=0;
+    for(int i=0;i<32;++i){					//time O(32n) = O(n)
+        int count = 0;
+	int offset = 1<<i;
+	for(int j=0;j<n;++j){
+            count += (arr[j] & offset) >>i;
+        }
+	if(count % 3 == 1)
+	    x |= 1<<i;
+    }
+    return x;
 }
 
 int main(int argc, char* argv[]){
-	string str;
-	while(1){
-		if(getline(cin, str) == 0 || str.empty())
-		  break;
-		int *arr = new int[str.size()]();
-		int leng = splitStr2IntArray(str, arr);
-		int x = findSingleNum01(arr, leng);
-		printf("the single number is %d\n", x);
-		delete[] arr;
-		arr = 0;
-	}
-	return 0;
+    string str;
+    while(1){
+        if(getline(cin, str) == 0 || str.empty())
+	    break;
+        int *arr = new int[str.size()]();
+        int leng = splitStr2IntArray(str, arr);
+        int x = findSingleNum01(arr, leng);
+        printf("the single number is %d\n", x);
+        delete[] arr;
+        arr = 0;
+    }
+    return 0;
 }
