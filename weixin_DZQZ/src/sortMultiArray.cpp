5d02b55624c956a894421fb12ef5a013dee9a788
teaspring <bruce.letter@gmail.com>
2013-10-02 20:33:12 +0800

    Update sortMultiArray.cpp
    reshape backspace

--- weixin_DZQZ/src/sortMultiArray.cpp
+++ weixin_DZQZ/src/sortMultiArray.cpp
@@ -15,57 +15,57 @@
 
 template<typename T>
 void myswap(T* left, T* right){
-	T tmp = *right;		//copy pointer
-	*right = *left;
-	*left = tmp;
-	tmp = 0;
+    T tmp = *right;		//copy pointer
+    *right = *left;
+    *left = tmp;
+    tmp = 0;
 }
 
 void sortTriArray(int* arr, int N){
-	int *p1=arr, *p2=0, *p3=arr+N-1;
-	while(p3 > p2){
-		while(p1<arr+N && (*p1)==1)
-		  p1++;
-		while(p3>=arr && (*p3)==3)
-		  p3--;
-		p2 = p1;
-		while(p2<arr+N && (*p2)==2)
-		  p2++;
-		if(p3<p2)	break;
-		if((*p2)==3)
-		  myswap<int>(p2, p3);
-		else
-		  myswap<int>(p2, p1);
-	}
+    int *p1=arr, *p2=0, *p3=arr+N-1;
+    while(p3 > p2){
+        while(p1<arr+N && (*p1)==1)
+	    p1++;
+        while(p3>=arr && (*p3)==3)
+	    p3--;
+        p2 = p1;
+        while(p2<arr+N && (*p2)==2)
+            p2++;
+        if(p3<p2)	break;
+        if((*p2)==3)
+            myswap<int>(p2, p3);
+        else
+            myswap<int>(p2, p1);
+    }
 }
 
 /*
  * mapping 1->2, 2->3, 3->5, all three are prime
  * */
 void sortOutputPrimeTriArray(int* arr, int N){
-	long multi = 1;
-	for(int i=0;i<N;i++){
-		if(arr[i] == 1)
-		  multi *= 2;
-		else if(arr[i] == 2)
-		  multi *= 3;
-		else if(arr[i] == 3)
-		  multi *= 5;
-	}
+    long multi = 1;
+    for(int i=0;i<N;i++){
+        if(arr[i] == 1)
+	    multi *= 2;
+        else if(arr[i] == 2)
+            multi *= 3;
+        else if(arr[i] == 3)
+            multi *= 5;
+    }
 	
-	while(multi%2 == 0){
-	  cout<<1<<" ";
-	  multi /= 2;
-	}
-	while(multi%3 == 0){
-	  cout<<2<<" ";
-	  multi /= 3;
-	}
-	while(multi%5 == 0){
-	  cout<<3<<" ";
-	  multi /= 5;
-	}
-	cout<<endl;
+    while(multi%2 == 0){
+        cout<<1<<" ";
+        multi /= 2;
+    }
+    while(multi%3 == 0){
+        cout<<2<<" ";
+        multi /= 3;
+    }
+    while(multi%5 == 0){
+        cout<<3<<" ";
+        multi /= 5;
+    }
+    cout<<endl;
 }
 
 /*
@@ -73,52 +73,51 @@ void sortOutputPrimeTriArray(int* arr, int N){
  * move all elements equal to pivot to head of array
  * */
 int myQuickSort(int* arr, int N, int pivot){	//arr has element of ==pivot and >pivot
-	if(N<1) return 0;
-	int *left=arr-1, *cur=arr;
-	while(cur<arr+N){
-		if(*cur>pivot){
-			cur++;
-			continue;
-		}
-		left++;
-		myswap(left, cur);
-		cur++;
+    if(N<1) return 0;
+    int *left=arr-1, *cur=arr;
+    while(cur<arr+N){
+        if(*cur>pivot){
+            cur++;
+            continue;
 	}
-	return left-arr+1;
+        left++;
+        myswap(left, cur);
+        cur++;
+    }
+    return left-arr+1;
 }
 
 /*
  * idea of quick sort, apply multi rounds of quick sort
  * */
 void sortQuartArray(int* arr, int N){	//array of element of {1,2,3,4}
-	int *src = arr;
-	for(int i=1;i<4;i++){				//elements of {1,2,3,4}, sort by 1,2,3 seperately
-		int leng = myQuickSort(src, N, i);
-		src = src+leng;
-		N = N-leng;
-	}
+    int *src = arr;
+    for(int i=1;i<4;i++){		//elements of {1,2,3,4}, sort by 1,2,3 seperately
+        int leng = myQuickSort(src, N, i);
+        src = src+leng;
+        N = N-leng;
+    }
 }
 
 void outputArray(int* arr, int N){
-	for(int i=0;i<N;i++)
-	  cout<<arr[i]<<" ";
-	cout<<endl;
+    for(int i=0;i<N;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
 }
 
 int main(int argc, char* argv[]){
-	string str;
-	while(1){
-		if(getline(cin, str)==0 || str.empty())	
-		  break;
-		int* array = new int[str.size()]();
-		int length = splitStr2IntArray(str,array);
-		outputArray(array, length);
-		sortQuartArray(array, length);
-		outputArray(array, length);
+    string str;
+    while(1){
+        if(getline(cin, str)==0 || str.empty())	
+            break;
+        int* array = new int[str.size()]();
+        int length = splitStr2IntArray(str,array);
+        outputArray(array, length);
+        sortQuartArray(array, length);
+        outputArray(array, length);
 		
-		delete[] array;
-		array = 0;
-	}
-	return 0;
+        delete[] array;
+        array = 0;
+    }
+    return 0;
 }
-
