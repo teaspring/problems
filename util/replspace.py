8d45101169a78732fcd585e14c7d1e1f6b35d63f
teaspring <iceberg_yu@hotmail.com>
2013-11-11 00:04:30 +0800

    update replspace.py for recurse

--- util/replspace.py
+++ util/replspace.py
@@ -1,13 +1,24 @@
 import os, sys
 
+def replfile(f):
+    root,ext = os.path.splitext(f)
+    if ext=='.c' or ext=='.cpp' or ext=='.h' or ext=='.py':
+        # longname = os.path.join(folder, f)
+        os.system('sed -i ' + f + ' -e \'s/\t/    /g\'')
+
+
+def scandir(folder):
+    files=os.listdir(folder)
+    for f in files:
+        lname = os.path.join(folder, f)
+        if os.path.isdir(lname):
+            scandir(lname)
+        else:
+            replfile(lname)
+
+# main function
 if len(sys.argv) > 1:
     folder=sys.argv[1]
 else:
     folder='./'
-files=os.listdir(folder)
-for f in files:
-	root,ext = os.path.splitext(f)
-	if ext=='.c' or ext=='.cpp' or ext=='.h':
-		longname = os.path.join(folder, f)
-        os.system('sed -i ' + longname + ' -e \'s/\t/    /g\'')
-
+scandir(folder)
