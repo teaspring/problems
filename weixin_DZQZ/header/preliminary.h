eecef08af32722c0b2cca5911d5828c9ad371f48
teaspring <iceberg_yu@hotmail.com>
2013-11-12 10:30:53 +0800

    format tab to space

--- weixin_DZQZ/header/preliminary.h
+++ weixin_DZQZ/header/preliminary.h
@@ -15,23 +15,23 @@ using namespace std;
  * */
 int splitStr2IntArray(string& str, int *array){
     int leng = 0;
-    string::size_type  pos = 0;        //string::size_type and std::size_t are alternative
+    size_t pos = 0;        //string::size_type and std::size_t are alternative
     int minus = 1;
     while(pos<str.size()){    
         if(str[pos] == '-' && pos<(str.size()-1) && isdigit(str[pos+1])){        //support minus
-        minus = -1;
-    }else if(isdigit(str[pos])){
-        string::size_type begin = pos;
+            minus = -1;
+        }else if(isdigit(str[pos])){
+            string::size_type begin = pos;
             while(pos<str.size() && isdigit(str[pos]))    //at exit, pos is the one following last digit
-            pos++;
+                pos++;
             char* tmp = new char[pos-begin+1];
             for(string::size_type i=0;i<pos-begin;i++)
                 tmp[i] = str[i+begin];
             tmp[pos-begin] = '\0';
             array[leng++] = minus*atoi(tmp);
-        minus = 1;
+            minus = 1;
             delete[] tmp;
-        tmp = 0;
+            tmp = 0;
         }
         pos++;
     }
@@ -43,37 +43,43 @@ int splitStr2IntArrayExt(string& str, int *array){
     string::size_type  pos = 0;        //string::size_type and std::size_t are alternative
     int minus = 1;
     while(pos<str.size()){
-    if(str[pos] == '-' && pos<(str.size()-1) && isdigit(str[pos+1])){
-    minus = -1;
-    }else if(isdigit(str[pos])){
-    int val = 0;
-        while(pos<str.size() && isdigit(str[pos])){
-        char ch = str[pos];          
-        val = (val==0) ? atoi(&ch) : 10*val + atoi(&ch);    //it works
+        if(str[pos] == '-' && pos<(str.size()-1) && isdigit(str[pos+1])){
+            minus = -1;
+        }else if(isdigit(str[pos])){
+            int val = 0;
+            while(pos<str.size() && isdigit(str[pos])){
+                char ch = str[pos];          
+                val = (val==0) ? atoi(&ch) : 10*val + atoi(&ch);    //it works
+                pos++;
+            }
+            array[leng++] = minus*val;
+            minus = 1;
+        }
         pos++;
-    }
-    array[leng++] = minus*val;
-    minus = 1;
-      }
-      pos++;
     }
     return leng;
 }
 
 /*
-* time O(lgm) to get index m == lgn
+* time O(lgm) to get index m == lg(n), from ../src/logarithm.cpp 
 */
-int getindex(int num, int& t, int e =10){
-    int m=1, pre_m=0, pre_t=1;
+int log(long num, long& t, int e =10){
+    int m=1, pre_m=0;
+    long pre_t=1;
     t=e;
-    while(num/t < 1 || num/t > 9){
-        if(num/t > 9){
-            pre_t = t;
-            t *= t;
-            pre_m = m;
-            m *= 2;
+    while(num/t == 0 || num/t > e-1){
+        if(num/t > e-1){
+            if(t/pre_t >= pre_t){
+                pre_t = t;
+                t *= t;
+                pre_m = m;
+                m *= 2;
+            }else{
+                t = t * (long)sqrt(t/pre_t);
+                m = m + (m-pre_m)/2;
+            }
         }else{
-            t = pre_t * (int)sqrt(t/pre_t);
+            t = pre_t * (long)sqrt(t/pre_t);
             m = pre_m + (m-pre_m)/2;
         }
     }
