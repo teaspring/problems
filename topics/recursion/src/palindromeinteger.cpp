88a3595410a86c9125b3a5dc5ee4b467dc768405
teaspring <iceberg_yu@hotmail.com>
2013-12-14 22:53:33 +0800

    format space

--- topics/recursion/src/palindromeinteger.cpp
+++ topics/recursion/src/palindromeinteger.cpp
@@ -12,24 +12,24 @@
 using namespace std;
 
 bool ispalindrome(int n, int x, int m){
-	if(n)
-	  return ispalindrome(n/10, 10*x + n%10, m);
-	else
-	  return x == m;
+    if(n)
+      return ispalindrome(n/10, 10*x + n%10, m);
+    else
+      return x == m;
 }
 
 bool palindromeint(int n){
-	return ispalindrome(n, 0, n);
+    return ispalindrome(n, 0, n);
 }
 
 int main(int, char**){
-	string str;
-	while(1){
-		if(getline(cin, str)==0 || str.empty())
-		  break;
-		int n = atoi(str.c_str());
-		bool res = palindromeint(n);
-		cout<<(res ? "true" : "false")<<endl;
-	}
-	return 0;
+    string str;
+    while(1){
+        if(getline(cin, str)==0 || str.empty())
+          break;
+        int n = atoi(str.c_str());
+        bool res = palindromeint(n);
+        cout<<(res ? "true" : "false")<<endl;
+    }
+    return 0;
 }
