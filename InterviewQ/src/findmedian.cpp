4b37ebfaf293de14ce3fdfc22140d56b20425eac
teaspring <iceberg_yu@hotmail.com>
2014-01-08 00:31:37 +0800

    correct find median of sorted two arrays

--- InterviewQ/src/findmedian.cpp
+++ InterviewQ/src/findmedian.cpp
@@ -5,67 +5,133 @@
 * 1.case of m==n is in <IntroductionToAlgorithm> chapter9 median and order statistics, exercise 9.3-8
 * 2.case of m!= n is an interview problem
 * 
-* test data:
-* for m==n
-* {1,3,5,8,9,10}, {2,4,6,9,10,11}
-* {1,2,3,4,5,6}, {8,9,10,11,12,13}
-* {8,9,10,11,12,13}, {1,2,3,4,5,6}
+* test case:
+*
+* -----m==n------
+* {1}, {1}
+* {1,2}, {3,4}
+* {1,3}, {2,4}
+* {1,2}, {1,2}
+* {1,4}, {2,3}
 * 
-* for m != n
-* {1,3,5,8,9}, {2,4,6,7,10,11,12}
-* {1,1,1,1}, {2,4,5,5,5,5}
-* {7,8,9,10}, {1,2,3,4,5}
+* -----unnecessarily m equaling to n------
+* {1,3,5,8}, {2,4,6,7}
+* {1,1}, {2,3,4,5}
+* {}, {1,2,3,4}
+* {1,2,3}, {}
+* {1}, {1}
+* {1,2,3,5,6}, {4}
+* {1}, {2,3,4}
+* {1}, {2,3,4,5}
+* {1,2,3,4}, {5}
+* {2}, {1,3,4}
+* {3}, {1,2,4}
+* {1,2,3}, {4,5}
+* {6,7,8}, {1,2,3,4,5}
 */
 
 #include "../header/preliminary.h"
 
-inline int max(int a, int b){
-    return a>b ? a : b;
-}
-
-bool findmediansingle(int* A,int m, int* B,int n,int& res,int tag){  // find total median in array A
-    int u=m, v=0, k=0;
-    int mid = (m+n)/2;
-    while(v<u){               //[v,u)
-        k = (v+u)/2;
-        int j=(mid-1-k);      //index in B[]
-        if(j<-1){
-            u=k;              //reduce k to enlarge j
-        }else if(j==-1){      //edge case for m-n==1 !!!
-            break;
-        }else if(j>n-1){        //enlarge k to reduce j
-            v=k+1;            
-        }else if(B[j] <= A[k]){              //depends on B[j+1] ?
-            if(j==n-1 || B[j+1] >= A[k]){    //edge: either j==n-1 or A[k] between {B[j], B[j+1]}
-                break;
-            }else{            //B[j+1] < A[k], reduce k to enlarge j
-                u=k;
-            }
-        }else{                //enlarge k to reduce j
-            v = k+1;
-        }
-    }
-    if(v<u){
-        if(!tag){
-            int prev = B[n-1-k];
-            if(k>0 && A[k-1] > prev){
-                prev = A[k-1];
-            }
-            res = (A[k] + prev)/2;
-        }else{
-          res = A[k];
-        }
+/*
+ * sorted array A[m] and B[n]
+ * */
+bool findmediansingle(int *A, int m, int *B, int n, double& res, int tag){
+    if(m==0 && n==0){
+        return false;
+    }else if(n==0){
+        res = tag==1 ? A[m/2] : (double)(A[m/2] + A[m/2 - 1])/2;
         return true;
+    }else if(m==0){
+        res = tag==1 ? B[n/2] : (double)(B[n/2 -1] + B[n/2])/2;
+        return true;
+    }
+
+    int u=m, v=0, i=0;
+    int mid = (m+n)/2;
+    if(tag==0){
+        while(v<u){               //[v,u)
+            i = (v+u)/2;
+            int j=(mid-1-i);      //index in B[]
+            if(j<-1){
+                u=i;              //reduce k to enlarge j
+            }else if(j==-1){    //it means all elements in A[] less than A[i] add up to mid
+                if(B[0] >= A[i]){    //B[0] is above A[i], out of mid elements below A[i], A[i] is floor of the middle two
+                    int high = A[i];
+                    int low = i>0 ? A[i-1] : 0;
+                    res = (double)(high + low)/2;
+                    return true;
+                }
+                break;
+            }else if(j>n-1){    //enlarge k to reduce j
+                v=i+1;            
+            }else if(A[i] <= B[j]){    //we need to find the middle two elements among the total merged array
+                if(j==0 || A[i] >= B[j-1]){ //all elements less than A[i] and B[j] adds up to mid
+                    int low = A[i];     //since we focus binary cursor on i, A[i] is fixed as floor of the two middle
+                    int high = B[j];
+                    if(i<m-1 && A[i+1]<B[j]){  //A[i+1] is closer to A[i] then B[j]
+                        high = A[i+1];
+                    }
+                    res = (double)(low + high)/2;
+                    return true;
+                }else{
+                    v=i+1;    //enlarge i to reduce j
+                    continue;
+                }
+            }else{  //A[i] > B[j]
+                if(j==n-1 || A[i] <= B[j+1]){
+                    int high = A[i];    //now we fix A[i] as ceil of the two middle
+                    int low = B[j];
+                    if(i>0 && B[j] < A[i-1]){    //A[i-1] is closer to A[i] than B[j]
+                        low = A[i-1];
+                    }
+                    res = (double)(low+high)/2;
+                    return true;
+                }else{
+                    u = i;    
+                    continue;
+                }
+            }
+        }
+        return false;
     }else{
+        while(v<u){               //[v,u)
+            i = (v+u)/2;
+            int j=(mid-1-i);      //index in B[]
+            if(j<-1){
+                u=i;              //reduce k to enlarge j
+            }else if(j==-1){  //it means all elements in A[] less than A[i] add up to mid
+                if(B[0] >= A[i]){  //B[0] is out of the mid elements below A[i], A[i] is the middle of all
+                    res = (char)A[i];
+                    return true;
+                }
+                break;
+            }else if(j>n-1){    //enlarge k to reduce j
+                v=i+1;  
+            }else if(A[i] >= B[j]){    //1.A[i] and B[j] are middle two; 2.max(A[i], B[j]) is middle of all
+                if(j==n-1 || A[i] <= B[j+1]){
+                    res = (double)A[i];
+                    return true;
+                }else{
+                    u=i;    //reduce i
+                }
+            }else{
+                if(i==m-1 || B[j] <= A[i+1]){
+                    res = (double)B[j];        //we need max(A[i], B[j])
+                    return true;
+                }else{
+                    v=i+1;    //enlarge i
+                }
+            }
+        }
         return false;
     }
 }
 
-int findmedian(int* A, int m, int* B, int n){
-    int res = 0;
+double findmedian(int* A, int m, int* B, int n){
+    double res = 0.0;
     int tag=0;
     if((m+n)%2 == 1)
-      tag=1;            //A'[mid] is median
+      tag=1;            //1 for odd total count, 0  for even
     if(!findmediansingle(A,m,B,n,res,tag)){
         findmediansingle(B,n,A,m,res,tag);
     }
@@ -75,40 +141,47 @@ int findmedian(int* A, int m, int* B, int n){
 /*
  * for two arrays with same size
  * */
-bool findmediansingle(int* A, int* B, int n, int& res){  // find total median in array A
-    int u=n, v=0, k=0;
+bool findmediansingle(int* A, int* B, int n, double& res){  // find total median in array A
+    int u=n, v=0;
     while(v<u){        //[v,u)
-        k = (v+u)/2;
-        int j=(n-1-k);
-        if(j<0){
-            u=k;
-        }else if(B[j] <= A[k]){              //not sufficient !
-            if(j==n-1 || B[j+1] >= A[k]){    //edge: either j==n-1 or A[m] between {B[j], B[j+1]}
-                break;
-            }else{                                //B[j+1] < A[k], reduce k to enlarge j
-                u=k;
+        int i = (v+u)/2;
+        int j = n-1-i;      //index in B[]
+        if(A[i] <= B[j]){    //we need to find the middle two elements among the total merged array
+            if(j==0 || A[i] >= B[j-1]){ //all elements less than A[i] and B[j] adds up to mid
+                int low = A[i];     //since we focus binary cursor on i, A[i] is fixed as floor of the two middle
+                int high = B[j];
+                if(i<n-1 && A[i+1]<B[j]){  //A[i+1] is closer to A[i] then B[j]
+                    high = A[i+1];
+                }
+                res = (double)(low + high)/2;
+                return true;
+            }else{
+                v=i+1;    //enlarge i to reduce j
+                continue;
             }
-        }else{                                //enlarge k to reduce j
-            v = k+1;
+        }else{  //A[i] > B[j]
+            if(j==n-1 || A[i] <= B[j+1]){
+                int high = A[i];    //now we fix A[i] as ceil of the two middle
+                int low = B[j];
+                if(i>0 && B[j] < A[i-1]){    //A[i-1] is closer to A[i] than B[j]
+                    low = A[i-1];
+                }
+                res = (double)(low+high)/2;
+                return true;
+            }else{
+                u = i;    
+                continue;
+            }
         }
     }
-    if(v<u){
-        int prev = B[n-1-k];
-        if(k>0 && A[k-1] > prev){
-            prev = A[k-1];
-        }
-        res = (A[k] + prev)/2;
-        return true;
-    }else{
-        return false;
-    }
+    return false;
 }
 
 /*
  * for two arrays with same size
  * */
-int findmedian(int* A, int* B, int n){
-    int res = 0;
+double findmedian(int* A, int* B, int n){
+    double res = 0.0;
     if(!findmediansingle(A, B, n, res)){
         findmediansingle(B, A, n, res);
     }
@@ -118,23 +191,21 @@ int findmedian(int* A, int* B, int n){
 int main(int argc, char* argv[]){
     string str;
     while(1){
-        if(getline(cin, str)==0 || str.empty()){
-            break;
-        }
+        cout<<"input 1st sorted int array:"<<endl;
+        getline(cin, str);
         int* arr1 = new int[str.size()]();
         int m = splitStr2IntArray(str, arr1);
     
         str.clear();
-        if(getline(cin, str)==0 || str.empty()){
-            break;
-        }
+        cout<<"input 2nd sorted int array:"<<endl;
+        getline(cin, str);
         int* arr2 = new int[str.size()]();
         int n = splitStr2IntArray(str, arr2);
-        //int r = findmedian(arr1, arr2, n);
-        //printf("the median of all the integers is %d\n", r);
+        //double r = findmedian(arr1, arr2, n);
+        //printf("the median of all the integers is %.1f\n", r);
         
-        int r = findmedian(arr1,m,arr2,n);
-        printf("the median of all the integers is %d\n", r);        
+        double r = findmedian(arr1,m,arr2,n);
+        printf("the median of all the integers is %.1f\n", r);        
         
         delete[] arr1;
         arr1 = 0;
