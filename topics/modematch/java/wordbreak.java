fbfc412149cf3bc157977cf093983e6048b908f1
teaspring <iceberg_yu@hotmail.com>
2014-01-12 17:34:52 +0800

    update

--- topics/modematch/java/wordbreak.java
+++ topics/modematch/java/wordbreak.java
@@ -121,7 +121,6 @@ public class wordbreak{
         for(int i=0;i<n;++i){
             arr.add(i, new ArrayList<Integer>());            
         }
-        /*
         for(int stop=n;stop>=0;--stop){
             if(stop<n && arr.get(stop).isEmpty())
                 continue;   //key and most smart statement, skip the index which can not arrive in end
@@ -130,17 +129,7 @@ public class wordbreak{
                     arr.get(start).add(stop);
                 }
             }
-        }
-        */
-        for(int start=0;start<n;++start){
-            if(start>0 && !arr.get(0).contains(start))
-                continue;   //key and most smart statement, skip the index which can not start from 0 to it 
-            for(int stop = start+1;stop<=n;++stop){
-                if(dict.contains(s.substring(start,stop))){
-                    arr.get(start).add(stop);
-                }
-            }
-        }
+        }      
         collect(arr, 0, s, "", res);
         return res;
     }
