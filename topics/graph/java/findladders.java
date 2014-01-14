22eb4f0c42ea6893f39569e1b2236858ffa59ddf
teaspring <iceberg_yu@hotmail.com>
2014-01-14 13:38:27 +0800

    add find word ladders

---
+++ topics/graph/java/findladders.java
@@ -0,0 +1,287 @@
+/*
+ * from oj.leetcode.
+ * given two words(start and end), and a dictionary, find all shortest transformation sequence(s) from start to end, such that:
+ * 1.only one letter can be chanegd at a time
+ * 2.each intermediate word must exist in the dictionary
+ *
+ * test data:
+ * start="hit", end="cog", dict=["hot","dot","dog","lot","log"]
+ * return [[hit,hot,dot,dog,cog], [hit,hot,lot,log,cog]]
+ * */
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class findladders{
+    private final int X = 0xffff;
+    /*
+    * Dijkstra algorithm. basically it is correct.in case the dict the conversion is unavailable, and the "start" is long, this solution is
+    * good. But if in case the "start" is not long while dict has too much redundent string, this is worse than _02. after all, it is not
+    * worth to create a adjcent matrix of all string in dict
+    * */
+    public int ladderLength_01(String start, String end, HashSet<String> dict){
+        if(start.equals(end)){
+            return 1;
+        }else if(canConvert(start, end)){
+            return 2;
+        }
+
+        if(dict.isEmpty()){
+            return 0;
+        }
+        if(dict.contains(start)){
+            dict.remove(start);
+        }
+        if(dict.contains(end)){
+            dict.remove(end);
+        }
+        ArrayList<String> strArr = new ArrayList<String>();
+        strArr.add(start);
+        for(String s : dict){
+            strArr.add(s);
+        }
+        strArr.add(end);
+        ArrayList<ArrayList<Integer>> adjMtx = getAdjMatrix(strArr);
+
+        final int n = strArr.size();
+        int[] dist = new int[n];
+        for(int i=0;i<n;i++){
+            dist[i] = X;
+        }
+        LinkedList<Integer> q = new LinkedList<Integer>();        //used as queue
+        boolean[] cover = new boolean[n];
+        for(boolean b : cover){
+            b = false;
+        }
+        q.addFirst(0);
+        dist[0] = 0;
+        
+        while(!q.isEmpty()){        //Dijkstra algorithm, shortest path from src vertex to dst
+            int u = q.pollLast();
+            cover[u] = true;
+            for(int v=0;v<n;v++){
+                if(v==u || adjMtx.get(u).get(v) != 1)        continue;
+                if(dist[u] + 1 < dist[v]){
+                    dist[v] = dist[u] + 1;
+                }
+                if(!q.contains(v) && cover[v] == false)    q.addFirst(v);
+            }
+        }
+        if(dist[n-1]==X)    return 0;        //dict[n-1]+1 is the ladder length
+        return dist[n-1]+1;
+    }
+    
+    public ArrayList<ArrayList<String>> findall_01(String start, String end, HashSet<String> dict){
+        ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();    
+        if(start.equals(end) || canConvert(start, end)){
+            ArrayList<String> arr = new ArrayList<String>();
+            arr.add(start);
+            arr.add(end);
+            res.add(arr);
+            return res;
+        }
+        if(dict.isEmpty()){
+            return res;
+        }
+        if(dict.contains(start)){
+            dict.remove(start);
+        }
+        if(dict.contains(end)){
+            dict.remove(end);
+        }
+        ArrayList<String> strArr = new ArrayList<String>();
+        strArr.add(start);
+        for(String s : dict){
+            strArr.add(s);
+        }
+        strArr.add(end);
+        ArrayList<ArrayList<Integer>> adjMtx = getAdjMatrix(strArr);
+
+        final int n = strArr.size();
+        ArrayList<HashSet<Integer>> prevPos = new ArrayList<HashSet<Integer>>();
+        for(int i=0;i<n;i++){
+            prevPos.add(new HashSet<Integer>());
+        }
+        int[] dist = new int[n];
+        for(int i=0;i<n;i++){
+            dist[i] = X;
+        }
+        LinkedList<Integer> q = new LinkedList<Integer>();        //used as queue
+        boolean[] cover = new boolean[n];
+        for(boolean b : cover){
+            b = false;
+        }
+        q.addFirst(0);
+        dist[0] = 0;
+        
+        while(!q.isEmpty()){
+            int u = q.pollLast();
+            cover[u] = true;
+            for(int v=0;v<n;v++){
+                if(v==u || adjMtx.get(u).get(v) != 1)        continue;
+                if(dist[u] + 1 < dist[v]){
+                    dist[v] = dist[u] + 1;
+                    prevPos.get(v).clear();
+                    prevPos.get(v).add(u);
+                }else if(dist[u] + 1 == dist[v] && !prevPos.get(v).contains(u)){
+                    prevPos.get(v).add(u);
+                }
+                if(!q.contains(v) && cover[v] == false)    q.addFirst(v);
+            }
+        }
+        if(dist[n-1]==X)    return res;        //dict[n-1]+1 is the ladder length
+        
+        LinkedList<Integer> stk = new LinkedList<Integer>();    //as stack
+        replayPath(prevPos, strArr, n-1, stk, res);
+        return res;
+    }
+
+    public ArrayList<ArrayList<Integer>> getAdjMatrix(ArrayList<String> strArr){    //assume src vertex is [0] and dest is [n-1]
+        final int n = strArr.size();
+        ArrayList<ArrayList<Integer>>    adjMtx = new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<n;++i){
+            adjMtx.add(new ArrayList<Integer>(n));
+            for(int j=0;j<n;++j){
+                if(i==j){
+                    adjMtx.get(i).add(0);
+                }else{
+                    adjMtx.get(i).add(X);
+                }
+            }
+        }
+        for(int u=0;u<n;++u){
+            for(int v=u+1;v<n;++v){
+                if(canConvert(strArr.get(u), strArr.get(v))){
+                    adjMtx.get(u).set(v,1);
+                    adjMtx.get(v).set(u,1);
+                }else{
+                    adjMtx.get(u).set(v,X);
+                    adjMtx.get(v).set(u,X);
+                }
+            }
+        }
+        return adjMtx;
+    }
+
+    public boolean canConvert(String s1, String s2){
+        if(s1.length() != s2.length())    return false;
+        int diff=0;
+        for(int i=0;i < s1.length();++i){
+            if(s1.charAt(i) != s2.charAt(i))
+                diff++;
+        }
+        return diff==1;
+    }
+
+    public void replayPath(ArrayList<HashSet<Integer>> prevPos, ArrayList<String> strArr, int ind, 
+            LinkedList<Integer> stk1, ArrayList<ArrayList<String>> res){
+        if(ind==0){
+            ArrayList<String> ladder = new ArrayList<String>();
+            ladder.add(strArr.get(0));
+            LinkedList<Integer> stk2 = new LinkedList<Integer>();
+            while(!stk1.isEmpty()){
+                int i = stk1.pop();
+                stk2.push(i);
+                ladder.add(strArr.get(i));
+            }
+            res.add(ladder);
+            while(!stk2.isEmpty()){
+                stk1.push(stk2.pop());
+            }
+            return;
+        }else{
+            stk1.push(ind);
+            for(int i : prevPos.get(ind)){
+                replayPath(prevPos, strArr, i, stk1, res);
+            }
+            stk1.pop();
+        }
+        return;
+    }
+
+    private static HashSet<String> parseStrArray(String str){
+        HashSet<String> st = new HashSet<String>();
+        StringTokenizer t = new StringTokenizer(str, " ,");
+        while(t.hasMoreTokens()){
+            String s = t.nextToken().toString();
+            if(!st.contains(s)){
+                st.add(s);
+            }
+        }
+        return st;
+    }
+
+    /*
+     * this solution considers every candicate which changes one char for once.
+     * 1. in case the dict has too much redundent string, and the size of "start" is not large, this is more efficient than _01
+     * 2. in frequent use, char[] is faster than String manipulation
+     * 3. Java Doc suggests ArrayDeque is faster than LinkedList in most cases
+     *
+     * note: this is accepted on oj. but actually, the structure steps can be replaced by several variables... 
+     * */
+    public int ladderLength_02(String start, String end, HashSet<String> dict){
+        if(start.equals(end))         return 1;
+        if(canConvert(start, end))    return 2;
+        if(dict.isEmpty())            return 0;
+
+        Set<String> visited = new HashSet<String>();
+        Queue<String> q = new ArrayDeque<String>();
+        q.add(start);
+        Queue<Integer> steps = new LinkedList<Integer>();
+        steps.add(0);
+        final int N = start.length();
+
+        while(!q.isEmpty()){
+            String word = q.poll();
+            visited.add(word);
+            int stp = steps.poll();
+            char[] wordChar = word.toCharArray();        //it is faster than manipulation of String
+            for(int i=0;i<N;++i){
+                char saved = wordChar[i];
+                for(char c='a';c <= 'z';++c){
+                    int st = stp;
+                    wordChar[i] = c;
+                    String str = new String(wordChar);
+                    if(str.equals(end))        return st+2;
+                    if(dict.contains(str) && !visited.contains(str) && !q.contains(str)){
+                        q.add(str);
+                        st++;
+                        steps.add(st);
+                    }
+                }
+                wordChar[i] = saved;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        findladders lad = new findladders();
+        while(true){            
+            System.out.println("input start string:");
+            String start = scan.nextLine().trim();
+            if(start.length()==0)        break;
+            
+            System.out.println("input end string:");
+            String end = scan.nextLine().trim();
+            if(end.length()==0)        break;
+            
+            System.out.println("input dictionary strings:");
+            String dictStr = scan.nextLine();
+            if(dictStr.length()==0)        break;
+            HashSet<String> dict = parseStrArray(dictStr);
+            
+            int steps = lad.ladderLength_02(start, end, dict);
+            System.out.println("the shortest ladder is " + steps);
+            /*
+            ArrayList<ArrayList<String>> result = lad.findall(start, end, dict);
+            for(int i=0;i<result.size(); i++){
+                System.out.println(result.get(i).toString());
+            }
+            */
+        }
+    }
+}
+
