f88889072ffe456f2756490228c6090906200941
teaspring <iceberg_yu@hotmail.com>
2014-01-19 08:35:10 +0800

    update KMP

--- topics/modematch/src/KMP.cpp
+++ topics/modematch/src/KMP.cpp
@@ -1,6 +1,11 @@
 /*
  * one copy of implementation of KMP string match algorithm
  * for string match problem that match a string T with pattern P, the KMP(Knuth-Morris-Pratt) algorithm is popular at its linear time O(n)
+ * 
+ * test data:    pattern/string
+ * abab/abbaabbaabab
+ * ababa/abaaba
+ * aaaaa/aabaaaabaaaaa
  * */
 #include "stdio.h"
 #include <iostream>
@@ -23,12 +28,15 @@ void setPrefix(const string& pattern, int *prefix){        //prefix should be [n
     for(int i=1;i<n;i++){
         int k=prefix[i-1];    //because index is 0-based, and length is 1-based. so k=prefix[i-1] is just the next potential matched char
         for(;pattern[k] != pattern[i] && k>0; k=prefix[k-1]);
-        if(pattern[k] == pattern[i])    prefix[i] = k+1;    //here k is index of 0-based, so prefix[i] should be 1-based
+        if(pattern[k] == pattern[i])    prefix[i] = k+1;    //here k is index of 0-based, prefix[i] should be 1-based, prefix[i]<=i
         else                            prefix[i] = 0;
     }
     return;
 }
 
+/*
+ * in total, KMP is in time O(n+m) while n is length of string to match, m is length of pattern
+ * */
 bool isMatchKMP(const string& str, const string& pattern){
     int n=str.size(), m=pattern.size();
     if(n<m)             return false;
@@ -39,16 +47,18 @@ bool isMatchKMP(const string& str, const string& pattern){
     int i=0, s=0;
     bool bfind = false;
     while(i<n){
-        if(s>i||(i-s<m && pattern[i-s] == str[i])){
+        int k=i-s;    //k is 0-based
+        if(k<0||(k<m && pattern[k] == str[i])){
             ++i;
-        }else if(i-s==m){
+        }else if(k==m){
             bfind=true;
             break;
-        }else{    //shift pattern at least 1
-            s += (i-s<=1)? 1 : max(1, prefix[i-s-1]);
+        }else{    //prefix[k] <=k
+            s += (k<1)? 1 : k - prefix[k-1];    //note, shift pattern either 1 or k-prefix[k-1] !!!
         }
     }
     delete[] prefix;
+    //printf("i=%d, s=%d\n", i, s);
     return bfind || i-s==m;     //note if i reaches n, it must be detected as well
 }
 
