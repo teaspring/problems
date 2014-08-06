2fb7cdd5a8db44c46641111779bb714c39f256b6
teaspring <teaspring@github.com>
2014-08-07 07:02:33 +0800

    merge unittest code to src file

--- leetcode/py/largestRectangleArea.py
+++ leetcode/py/largestRectangleArea.py
@@ -1,4 +1,5 @@
-import functools
+# how to execute: >>> python -m unittest largestRectangleArea
+import unittest 
 
 class Solution:
     # @param height, a list of integer
@@ -28,3 +29,20 @@ class Solution:
                     res = curr
             stk.append(i)
         return res
+
+# Not in need now: from largestRectangleArea import Solution
+
+class TestLRA(unittest.TestCase):
+    def test_execute(self):  # each test case must be named as text_xxx
+        s = Solution()
+        a = s.largestRA([2,1,3,1,4,2])
+        self.assertEquals(a, 6)
+
+        a = s.largestRA([2,1,5,6,2,3])
+        self.assertEquals(a, 10)
+
+        a = s.largestRA([0,0,0,0,0,0,11])
+        self.assertEquals(a, 11)
+
+if __name__ == '__main__':
+    unittest.main()
