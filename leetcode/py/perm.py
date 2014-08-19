5ffe11074940a545fd8c58f5e3e4c3cafcaf919d
teaspring <teaspring@github.com>
2014-08-19 17:07:57 +0800

    update permutation for recurse solution

--- leetcode/py/perm.py
+++ leetcode/py/perm.py
@@ -4,6 +4,7 @@
 # permutation and combination
 
 import os, sys
+from copy import copy
 
 # generate permutation
 def perm(items, n=None):
@@ -18,6 +19,30 @@ def perm(items, n=None):
             for p in perm(rest, n-1):       # recurse
                 yield v + p
 
+def swaplist(A, i, j):
+    if len(A) <= i or len(A) <= j or i==j:
+        return
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+
+# recurse for C++ style
+def perm_02(items):
+    res = []
+    res.append(copy(items))   # initial status must be processed precedingly
+    recur(items, 0, len(items), res)
+    return res
+
+def recur(items, start, end, res):
+    if start >= end:
+        return
+    recur(items, start+1, end, res)
+    for i in range(start+1, end):
+        swaplist(items, start, i)
+        res.append(copy(items))    # shallow copy
+        recur(items, start+1, end, res)
+        swaplist(items, start, i)
+
 def comb(items, n=None):
     if n is None:
         n = len(items)
@@ -30,25 +55,34 @@ def comb(items, n=None):
             for c in comb(rest, n-1):
                 yield v + c
 
-# main function
-if len(sys.argv) < 2:
-    items = '1234'
-else:
-    items = sys.argv[1]
+def test_01():
+    if len(sys.argv) < 2:
+        items = '1234'
+    else:
+        items = sys.argv[1]
 
-if len(sys.argv) < 3:
-    n = None
-else:
-    n = int(sys.argv[2])
+    if len(sys.argv) < 3:
+        n = None
+    else:
+        n = int(sys.argv[2])
 
-ps = perm(items, n)
-print 'permutation:'
-for p in ps:
-    print p
-print '-'*20
+    ps = perm(items, n)
+    print 'permutation:'
+    for p in ps:
+        print p
+    print '-'*20
 
-cs = comb(items, n)
-print 'combination:'
-for c in cs:
-    print c
+    cs = comb(items, n)
+    print 'combination:'
+    for c in cs:
+        print c
 
+def test_02():
+    res = perm_02(range(4))
+    for p in res:
+        print p
+    print len(res)
+
+if __name__ == '__main__':
+    # test_01()
+    test_02()
