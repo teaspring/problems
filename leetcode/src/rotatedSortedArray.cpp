1a22ec79682d56df4fc441c2cf485b5a84c6ac3d
teaspring <teaspring@github.com>
2014-10-29 12:58:32 +0800

    minimum of rotated sorted array II

--- leetcode/src/rotatedSortedArray.cpp
+++ leetcode/src/rotatedSortedArray.cpp
@@ -1,11 +1,14 @@
 /*
- * search in a sorted(asending) array which is rotated somewhere, initially in ascending order
+ * search in a sorted(asending) array which is rotated somewhere, assume it is ascending order initially
  *
  * test case:
  * 5,1,3, x=4, return -1
  * 5,6,1,2,3,  x=5, return 0
  * */
 #include "../include/preliminary.h"
+
+class Solution{
+public:
 
 int search(int A[], int n, int key){  // problem I: no duplicate
     int l=0, r=n-1;
@@ -55,20 +58,90 @@ int searchII(int A[], int n, int key){  // probelm II: with duplicates
     return -1;
 }
 
-int main(int, char**){
+int findMin(vector<int>& nums){  // find minimum of a rotated sorted array
+    int n = nums.size();
+    if(n == 0)    return 0;
+    if(n == 1)    return nums[0];
+    int l = 0, r = n-1;
+    while(l < r){
+        if(r == l+1)    break;    // include case of m==0
+        if(nums[l] < nums[r])    break;  // no rotate at all
+        int m = l + (r-l)/2;
+        if(nums[m-1] > nums[m])    return nums[m];
+        if(nums[l] < nums[m]){ 
+            l = m+1;
+        }else{
+            r = m-1;
+        }
+    }
+    return min(nums[l], nums[r]);
+}
+
+int findMinII(vector<int>& nums){  // duplicate exists in array
+    int n = nums.size();
+    if(n == 0)    return 0;
+    if(n == 1)    return nums[0];
+    int l = 1, res = nums[0], r = n-1;
+    while(l < r){
+        if(r == l+1)    break;
+        int m = l + (r-l)/2;
+        if(nums[m-1] > nums[m])    return min(res, nums[m]);
+        if(nums[l] == nums[m]){
+            res = min(min(nums[m], nums[r]), res);
+            l++;
+        }else if(nums[r] == nums[m]){
+            res = min(min(nums[l], nums[m]), res);
+            r--;
+        }else if(nums[l] == nums[r]){
+            res = min(nums[m], res);
+            l++;
+            r--;
+        }else if(nums[l] < nums[m]){
+            res = min(res, nums[l]);
+            l = m+1;
+        }else{
+            res = min(res, nums[m]);
+            r = m;
+        }
+    }
+    return min(res, min(nums[l], nums[r]));
+}
+
+};
+
+void test_01(){
     string str;
     printf("please input sorted ascending array which is rotated in some where\n");
-    if(getline(cin, str)==0 || str.empty())
-      return 0;
+    if(getline(cin, str)==0 || str.empty())    return;
     int *arr = new int[str.size()]();
     int n = splitStr2IntArray(str, arr);
+    Solution s;
     while(1){    
         printf("please input target to search:\n");
         if(getline(cin, str)==0 || str.empty())        break;
         int target = atoi(str.c_str());
-        printf("index of target is %d\n", search(arr, n, target));
-        
+        printf("index of target is %d\n", s.search(arr, n, target));
     }
     delete[] arr;
+    return;
+}
+
+void test_02(){
+    Solution s;
+    while(1){
+        string str;
+        printf("please input rotated integer array\n");
+        if(getline(cin, str) == 0 || str.empty())    break;
+        int arr[str.size()];
+        int n = splitStr2IntArray(str, arr);
+        vector<int> vec;
+        for(int i=0; i<n; i++)    vec.push_back(arr[i]);
+        printf("%d\n", s.findMinII(vec));
+    }
+    return;
+}
+
+int main(int, char**){
+    test_02();
     return 0;
 }
