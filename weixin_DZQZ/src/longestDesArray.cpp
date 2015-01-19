e1896a04484734ede48ac3172f4c78e8900e3ee0
teaspring <teaspring@github.com>
2015-01-19 15:35:01 +0800

    add one mether

--- weixin_DZQZ/src/longestDesArray.cpp
+++ weixin_DZQZ/src/longestDesArray.cpp
@@ -21,9 +21,9 @@ int longestDesArray(int *arr, int n, int asc, int& end){
         int j = i-1;
         while(j>=0){
             if(((asc && arr[i] >= arr[j]) || (!asc && arr[i] <= arr[j]))    // equation belongs to ascending/descending
-                && plength[i] < plength[j]+1){
-                plength[i] = plength[j]+1;
-                if(plength[i] > res){       //longest ascending/descending array till now.its size increases by 1 mostly, break is OK  
+                && plength[i] < plength[j]+1){ // just compare plength[i-1] and plength[i]
+                plength[i] = plength[j]+1;  // bring in excessive update of plength[i]
+                if(plength[i] > res){
                     res = plength[i];
                     end = i;
                     break;
@@ -37,19 +37,44 @@ int longestDesArray(int *arr, int n, int asc, int& end){
     return res;
 }
 
+int longestAscArray(int *arr, int n){
+    int f[n]; // f[i] is length of longest ascending array in arr[0...i]
+    int end[n]; // end[i] is end index of longest ascending array in arr[0...i]
+    f[0] = 1;
+    end[0] = 0;
+    for(int i=1; i<n; i++){
+        f[i] = f[i-1];
+        end[i] = end[i-1];
+        if(arr[end[i-1]] < arr[i]){
+            f[i]++;
+            end[i] = i;
+            continue;
+        }
+        for(int j=i-1; j>=0; --j){
+            if(j+1 < f[i])    break; // further preceding array has no means
+            if(f[j] < f[i] || arr[end[j]] >= arr[i])    continue;
+            f[i] = f[j] + 1;
+            end[i] = i;
+        }
+    }
+    return f[n-1];
+}
+
 int main(int argc, char* argv[]){
     string str;
     while(1){
-        if(getline(cin, str) == 0 || str.empty())
-          break;
+        if(getline(cin, str) == 0 || str.empty())  break;
         int *arr = new int[str.size()]();
         int n = splitStr2IntArray(str, arr);
-        int e = 0;
-        int leng = longestDesArray(arr, n, 1, e);
-        printf("the longest ascending sub array is %d, ends at [%d]\n", leng, e);
+        int e = 0, leng = 0;
+        leng = longestDesArray(arr, n, 1, e);
+        printf("version1: the longest ascending array is %d, ends at [%d]\n", leng, e);
+        leng = longestAscArray(arr, n);
+        printf("version2: the longest ascending sub array is %d\n", leng);
+        /*
         leng = longestDesArray(arr, n, 0, e);
         printf("the longest descending sub array is %d, ends at [%d]\n", leng, e);
-        
+        */
         delete[] arr;
         arr = 0;   
     }
