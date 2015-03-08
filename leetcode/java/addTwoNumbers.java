81b6211abe5024e9c96b16b441e51b40ec48b6f9
teaspring <teaspring@github.com>
2015-03-08 12:41:28 +0800

    addTwoNumbers() support negative number:w

--- leetcode/java/addTwoNumbers.java
+++ leetcode/java/addTwoNumbers.java
@@ -4,10 +4,15 @@
  * 8->9    + 2->3->4->5
  * 1->9->9 + 2->4->3
  *
- * @extend: what about negative value of node? the requisite is define a format to display negative value in list node
+ * @extend: what about negative value of node?
+ * firstly, we can define a format: -15 is presented via list node as (-5) -> (1)
+ * we have 2 kinds of cases:
+ * 1.both SLL are negative
+ * 2.only one if them is negative,
  * */
 import java.util.*;
 import java.io.*;
+import java.lang.Math;
 
 public class addTwoNumbers{
     public class ListNode{
@@ -32,6 +37,15 @@ public class addTwoNumbers{
     public ListNode addTwo(ListNode l1, ListNode l2){
         if(l1 == null)    return copyList(l2); //return a copy instead of argument directly!
         if(l2 == null)    return copyList(l1);
+        boolean bothNega = false;
+        if(l1.val < 0 && l2.val < 0){
+            bothNega = true;
+            l1.val = -l1.val;
+            l2.val = -l2.val;
+        }else if(l1.val < 0 || l2.val < 0){
+            return addTwo_nega(l1, l2);
+        }
+
         int bene = 0;
         ListNode head = null, curr = null;
         while(l1 != null || l2 != null){  //note: while following SLL remains, it cannot break as it needs to create new node
@@ -44,6 +58,7 @@ public class addTwoNumbers{
             }else{
                 bene = 0;
             }
+
             if(curr == null){
                 head = new ListNode(x);
                 curr = head;
@@ -56,9 +71,99 @@ public class addTwoNumbers{
         }
         if(bene != 0)    //remember if finally there is bene exist
             curr.next = new ListNode(bene);
+
+        if(bothNega){
+            head.val = -head.val;
+        }
         return head;
     }
 
+    /*
+     * one of lists (l1, l2) is negative
+     * */
+    public ListNode addTwo_nega(ListNode l1, ListNode l2){
+        /// compare abs value of list1(l1) and list2(l2)
+        /// two kinds of result: one has more decimals than the other, or one has greater value on highest digit
+        boolean l1Negative = l1.val < 0;
+        if(l1Negative){
+            l1.val = -l1.val;
+        }else{
+            l2.val = -l2.val;
+        }
+
+        int n1 = 0, n2 = 0;
+        ListNode h1 = l1, h2 = l2;
+        /// use Stack to store digits
+        Stack<Integer> stk1 = new Stack<Integer>();
+        Stack<Integer> stk2 = new Stack<Integer>();
+
+        /// decide abs(l2) - abs(l1) or opposite
+        while(h1 != null || h2 != null){
+            if(h1 != null){
+                n1++;
+                stk1.push(h1.val);
+                h1 = h1.next;
+            }
+            if(h2 != null){
+                n2++;
+                stk2.push(h2.val);
+                h2 = h2.next;
+            }
+        }
+
+        boolean l2GreaterAbs = false;
+        if(n1 < n2){
+            l2GreaterAbs = true;
+        }else if(n1 == n2){
+            while(!stk1.isEmpty() && stk1.peek() == stk2.peek()){
+                stk1.pop();
+                stk2.pop();
+            }
+            if(stk1.isEmpty())    return new ListNode(0);
+            l2GreaterAbs = stk1.peek() < stk2.peek();
+        }
+
+        /// calculate the minus arith operation
+        ListNode head = null, curr = null;
+        h1 = l1;
+        h2 = l2;
+        int borrow = 0;
+        while(l1 != null || l2 != null){
+            int x = -borrow;
+            if(l2GreaterAbs){
+                x += l2.val;
+                x -= (l1 == null) ? 0 : l1.val;
+            }else{
+                x += l1.val;
+                x -= (l2 == null) ? 0 : l2.val;
+            }
+
+            if(x < 0){
+                borrow = 1;
+                x += 10;
+            }else{
+                borrow = 0;
+            }
+
+            if(curr == null){
+                head = new ListNode(x);
+                curr = head;
+            }else{
+                curr.next = new ListNode(x);
+                curr = curr.next;
+            }
+            if(l1 != null)    l1 = l1.next;
+            if(l2 != null)    l2 = l2.next;
+        }
+        /// finally, borrow should not > 0
+
+        if((l1Negative && !l2GreaterAbs)
+        || (!l1Negative && l2GreaterAbs)){
+            head.val = -head.val;
+        }
+        return head;
+    }
+    /*
     private void display(ListNode head){
         if(head == null){
             System.out.println();
@@ -72,6 +177,7 @@ public class addTwoNumbers{
         }
         System.out.println();
     }
+    */
 
     /* unit test will be executed in ../java_unittest/addTwoNumbers_unittest/ */
 }
--- leetcode/java_unittest/addTwoNumbers_junit/addTwoNumbersUnitTest.java
+++ leetcode/java_unittest/addTwoNumbers_junit/addTwoNumbersUnitTest.java
@@ -15,16 +15,18 @@ public class addTwoNumbersUnitTest{
     }
 
     @Test
-    public void positiveTest(){
+    public void positiveTest_01(){
+        // a = 98
         addTwoNumbers.ListNode p1 = game.new ListNode(8);
         p1.next = game.new ListNode(9);
-
+        // b = 432
         addTwoNumbers.ListNode p2 = game.new ListNode(2);
         p2.next = game.new ListNode(3);
         p2.next.next = game.new ListNode(4);
 
         addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
 
+        // c = 530
         int[] arr = new int[]{0, 3, 5};
         addTwoNumbers.ListNode h = p3;
         for(int i = 0; i < arr.length; i++){
@@ -35,15 +37,36 @@ public class addTwoNumbersUnitTest{
     }
 
     @Test
+    public void positiveTest_02(){
+        // a = 8
+        addTwoNumbers.ListNode p1 = game.new ListNode(8);
+        // b = 5
+        addTwoNumbers.ListNode p2 = game.new ListNode(5);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
+
+        // c = 13
+        int[] arr = new int[]{3, 1};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
     public void edgeTest(){
+        // a = 19
         addTwoNumbers.ListNode p1 = game.new ListNode(9);
         p1.next = game.new ListNode(1);
-
+        // b = 87
         addTwoNumbers.ListNode p2 = game.new ListNode(7);
         p2.next = game.new ListNode(8);
 
         addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
 
+        // c = 106
         int[] arr = new int[]{6, 0, 1};
         addTwoNumbers.ListNode h = p3;
         for(int i = 0; i < arr.length; i++){
@@ -72,16 +95,161 @@ public class addTwoNumbersUnitTest{
     }
 
     @Test
-    public void negativeTest(){
-		/* now the addTwo() does not support negative valued node */
+    public void bothnegativeTest(){
+        // a = -5
         addTwoNumbers.ListNode p1 = game.new ListNode(-5);
-
-        addTwoNumbers.ListNode p2 = game.new ListNode(2);
-        p2.next = game.new ListNode(1);
+        // b = -8
+        addTwoNumbers.ListNode p2 = game.new ListNode(-8);
 
         addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
 
-        int[] arr = new int[]{7};
+        // c = -13
+        int[] arr = new int[]{-3, 1};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void negativeTest_01(){
+        // a = -95
+        addTwoNumbers.ListNode p1 = game.new ListNode(-5);
+        p1.next = game.new ListNode(9);
+        // b = 38
+        addTwoNumbers.ListNode p2 = game.new ListNode(8);
+        p2.next = game.new ListNode(3);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
+
+        // c = -57
+        int[] arr = new int[]{-7, 5};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void negativeTest_02(){
+        // a = -95
+        addTwoNumbers.ListNode p1 = game.new ListNode(-5);
+        p1.next = game.new ListNode(9);
+        // b = 38
+        addTwoNumbers.ListNode p2 = game.new ListNode(8);
+        p2.next = game.new ListNode(3);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p2, p1);
+
+        // c = -57
+        int[] arr = new int[]{-7, 5};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void negativeTest_11(){
+        // a = -81
+        addTwoNumbers.ListNode p1 = game.new ListNode(-1);
+        p1.next = game.new ListNode(8);
+        // b = 93
+        addTwoNumbers.ListNode p2 = game.new ListNode(3);
+        p2.next = game.new ListNode(9);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
+
+        // c = 12
+        int[] arr = new int[]{2, 1};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void negativeTest_12(){
+        // a = -81
+        addTwoNumbers.ListNode p1 = game.new ListNode(-1);
+        p1.next = game.new ListNode(8);
+        // b = 93
+        addTwoNumbers.ListNode p2 = game.new ListNode(3);
+        p2.next = game.new ListNode(9);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p2, p1);
+
+        // c = 12
+        int[] arr = new int[]{2, 1};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void zeroTest_01(){
+        // a = -81
+        addTwoNumbers.ListNode p1 = game.new ListNode(-1);
+        p1.next = game.new ListNode(8);
+        // b = 81
+        addTwoNumbers.ListNode p2 = game.new ListNode(1);
+        p2.next = game.new ListNode(8);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
+
+        // c = 0
+        int[] arr = new int[]{0};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void zeroTest_02(){
+        // a = -81
+        addTwoNumbers.ListNode p1 = game.new ListNode(-1);
+        p1.next = game.new ListNode(8);
+        // b = 0
+        addTwoNumbers.ListNode p2 = game.new ListNode(0);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p1, p2);
+
+        // c = -81
+        int[] arr = new int[]{-1, 8};
+        addTwoNumbers.ListNode h = p3;
+        for(int i = 0; i < arr.length; i++){
+            Assert.assertNotNull(h);
+            Assert.assertEquals(arr[i], h.val);
+            h = h.next;
+        }
+    }
+
+    @Test
+    public void zeroTest_03(){
+        // a = -81
+        addTwoNumbers.ListNode p1 = game.new ListNode(-1);
+        p1.next = game.new ListNode(8);
+        // b = 0
+        addTwoNumbers.ListNode p2 = game.new ListNode(0);
+
+        addTwoNumbers.ListNode p3 = game.addTwo(p2, p1);
+
+        // c = -81
+        int[] arr = new int[]{-1, 8};
         addTwoNumbers.ListNode h = p3;
         for(int i = 0; i < arr.length; i++){
             Assert.assertNotNull(h);
