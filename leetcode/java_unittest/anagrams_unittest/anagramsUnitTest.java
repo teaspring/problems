8cd3f8398576de0014d1b7a7215a5149e5d7e1c1
teaspring <bruce.letter@gmail.com>
2015-03-10 15:00:02 +0800

    add test case

--- leetcode/java_unittest/anagrams_unittest/anagramsUnitTest.java
+++ leetcode/java_unittest/anagrams_unittest/anagramsUnitTest.java
@@ -17,7 +17,7 @@ public class anagramsUnitTest{
     }
 
     @Test
-    public void positiveTest(){
+    public void positiveTest_01(){
         String arr[] = new String[]{"dog", "god", "dad", "add", "dod"};
         ArrayList<String> result = game.getAnagrams(arr);
 
@@ -26,6 +26,26 @@ public class anagramsUnitTest{
 
         Assert.assertTrue(expected.containsAll(result));
         Assert.assertTrue(result.containsAll(expected));
+    }
+
+    @Test
+    public void positiveTest_02(){
+        String arr[] = new String[]{"about", "batuo", "outba", "bat", "auto", "tabo"};
+        ArrayList<String> result = game.getAnagrams(arr);
+
+        String res[] = new String[]{"batuo", "outba", "about"};
+        ArrayList<String> expected = new ArrayList<String>(Arrays.asList(res));
+
+        Assert.assertTrue(expected.containsAll(result));
+        Assert.assertTrue(result.containsAll(expected));
+    }
+
+    @Test
+    public void zeroTest(){
+        String arr[] = new String[]{"ab", "b", "a", "ad", "bat"};
+        ArrayList<String> result = game.getAnagrams(arr);
+
+        Assert.assertTrue(result.isEmpty());
     }
 
     @After
