506409f7e714c21c0a0f4d0a893c5ee5832b4b73
teaspring <bruce.letter@gmail.com>
2015-03-19 09:26:59 +0800

    unit test of wordbreakII

--- leetcode/java/wordbreak.java
+++ leetcode/java/wordbreak.java
@@ -1,12 +1,16 @@
 /*
  * from oj.leetcode
- * problem 1: given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or
+ * problem I:
+ * given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or
  * more dictionary words.
  * brute force is recursion, better solution is dynamic programming in time O(n^2)
  * test data:
  * s=leetcode, dict={lee, leet, tcod, code}, true
  * s=leetcode, dict={lee, tco, d, leet, cod}, false
- * s=leetcode, dict={lee, cod, de, leetcode}
+ *
+ * problem II:
+ * given s and dict, add spaces in s to construct a sentence where each word is valid in dict. return all possible sentences
+ * note: word in dict can be used repeatly
  * */
 
 import java.io.*;
@@ -37,80 +41,6 @@ public class wordbreak{
     }
 
     /*
-     * correct but less efficient solution to problem 2. 
-     * first, a traverse of O(n^2) is necessary; second, as this problem needs to output all contained substring composition, dp[i][j]
-     * storing substr [i,j) whether contained is less efficient, some structure storing which substr stopping at j for those starting at i
-     * is contained is better.
-     * */
-    public ArrayList<String> wordbreakall_01(String s, Set<String> dict){
-        final int n = s.length();
-        ArrayList<String> res = new ArrayList<String>();
-        if(n==0 || dict.size()==0)    return res;
-        
-        int[][] dp = new int[n][n+1];    //dp[i][j] means substr among [i,j) whether is contained in dict
-        for(int i=0;i<n;++i){
-            for(int j=0;j<=n;++j){
-                dp[i][j] = 0;
-            }
-        }
-        Stack<Integer> stk1 = new Stack<Integer>();
-        Stack<Integer> stk2 = new Stack<Integer>();
-        int i=1,j=0;
-        try{
-        while(i<=n || !stk1.empty()){
-            j = stk1.empty()  ? 0 : stk1.peek();
-            if(i>n){
-                if(stk1.empty())    break;
-                i = stk1.pop() + 1;
-                continue;
-            }
-            if(dp[j][i]==0){
-                dp[j][i] = dict.contains(s.substring(j,i)) ? 1 : -1;
-            }
-            if(dp[j][i] == -1){
-                ++i;
-                continue;
-            }           
-            stk1.push(i);
-            j=i;
-            int t=i;
-            ++i;
-            while(i<=n){
-                if(dp[t][i]==0){
-                    dp[t][i] = dict.contains(s.substring(t,i)) ? 1 : -1;
-                }
-                if(dp[t][i]==1){
-                    stk1.push(i);
-                    t=i;
-                }
-                ++i;
-            }
-            if(stk1.peek()==n){
-                StringBuilder strBuilder = new StringBuilder();
-                while(!stk1.empty()){    //in help of stk2, output the elements in stk1 in order
-                    stk2.push(stk1.pop());
-                }
-                t=0;
-                while(!stk2.empty()){
-                    stk1.push(stk2.pop());
-                    strBuilder.append(s.substring(t, stk1.peek()));
-                    strBuilder.append(" ");
-                    t=stk1.peek();
-                }
-                res.add(strBuilder.toString().trim());
-                t = stk1.pop();        //n
-                if(stk1.empty())    break;
-            }
-            i = stk1.pop()+1;
-        }
-        }
-        catch(Exception exp){
-            System.out.println(exp.toString());
-        }
-        return res;
-    }
-
-    /*
      * efficient solution to problem 2.
      * 1. a traverse of O(n^2) is necessary and a List<List<int>> is used for the traverse result;
      * 2. utilize the structure to find all consecutive composition
@@ -120,21 +50,21 @@ public class wordbreak{
      * dict={a,aa,aaa,aaaa,aaaaa...}, str=baaaaaaaaaaaaaaa
      * think about how to skip duplicate parts during preparement(fill the List<List<int>>)
      * 
-     * why it can skip some steps in traverse? because the Q asks to find "word break" instead of "word occurance",
+     * why it can work to skip? because the Q asks to find "word break" instead of "word occurance",
      * any word occurance which cannot contribute to a complete breaking option does not need to cover
      * */
-    public ArrayList<String> wordbreakall_02(String s, Set<String> dict){
+    public ArrayList<String> wordbreakall(String s, Set<String> dict){
         ArrayList<String> res = new ArrayList<String>();
         int n = s.length();
         if(n == 0 || dict.isEmpty())    return res;
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>(n);
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>(); // arr[s] = e means substr[s, e) in dict
         for(int i = 0; i < n; ++i){
             arr.add(i, new ArrayList<Integer>());            
         }
-        for(int stop = n; stop > 0; --stop){ //as collect() start from [0] later, here traverse from [n-1] will skip more clauses?
+        for(int stop = n; stop > 0; --stop){ //as collect() start from [0] later, traverse from [n-1] will skip more clauses?
             if(stop < n && arr.get(stop).isEmpty())
-                continue;   //key and most smart statement, skip the index which can not arrive in end
-            for(int start = stop-1; start >= 0; --start){
+                continue;   // key statement to skip the stop which can not jump to end as start
+            for(int start = stop - 1; start >= 0; --start){
                 if(dict.contains(s.substring(start,stop))){
                     arr.get(start).add(stop);
                 }
@@ -146,10 +76,10 @@ public class wordbreak{
 
     protected void collect(ArrayList<ArrayList<Integer>> arr, int start, String str,
             String prev, ArrayList<String> res){
-        if(start >= arr.size())    return;
         for(int stop : arr.get(start)){
             String prefix = prev;
-            prefix += (start == 0 ? str.substring(start, stop) : " " + str.substring(start, stop));
+            prefix += (start == 0 ? "" : " ");
+            prefix += str.substring(start, stop);
             if(stop == str.length()){
                 res.add(prefix);
             }else{ 
--- leetcode/java_unittest/wordbreak_junit/wordbreakUnitTest.java
+++ leetcode/java_unittest/wordbreak_junit/wordbreakUnitTest.java
@@ -2,8 +2,13 @@
  * wordbreakI:
  * boolean wordbreak.canBreak(String s, Set<String>)
  *
+ * wordbreakII:
+ * ArrayList<String> wordbreak.wordbreakall(String s, Set<String> dict)
+ *
  * */
 
+import java.util.List;
+import java.util.Collection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -26,9 +31,7 @@ public class wordbreakUnitTest{
 
         String[] arr_succ = new String[]{"lee", "tco", "d", "leet", "code"};
         Set<String> dict_succ = new HashSet<String>();
-        for(String s : arr_succ){
-            dict_succ.add(s);
-        }
+        fillCollection(arr_succ, dict_succ);
 
         Assert.assertTrue(game.canBreak(S, dict_succ));
     }
@@ -39,9 +42,7 @@ public class wordbreakUnitTest{
 
         String[] arr_fail = new String[]{"lee", "tco", "d", "oe", "cod", "leet"};
         Set<String> dict_fail = new HashSet<String>();
-        for(String s : arr_fail){
-            dict_fail.add(s);
-        }
+        fillCollection(arr_fail, dict_fail);
 
         Assert.assertFalse(game.canBreak(S, dict_fail));
     }
@@ -54,29 +55,63 @@ public class wordbreakUnitTest{
         Assert.assertTrue(game.canBreak(S, dict));
     }
 
-    private ArrayList<String> fillList(String[] arr){
-        ArrayList<String> dst = new ArrayList<String>();
-        for(int i=0; i < arr.length; i++){
-            dst.add(arr[i]);
-        }
-        return dst;
+    @Test
+    public void wordbreakII_positiveTest_01(){
+        String s = new String("catsanddog");
+        String[] arr = new String[]{"cat", "cats", "and", "sand", "dog"};
+        Set<String> dict = new HashSet<String>();
+        fillCollection(arr, dict);
+        List<String> result = game.wordbreakall(s, dict);
+
+        String[] expectedArr = new String[]{"cats and dog", "cat sand dog"};
+        List<String> expected = new ArrayList<String>();
+        fillCollection(expectedArr, expected);
+        assertListExt(expected, result);
     }
 
-    /*
-     * to assert two ArrayList<ArrayList<String>> objects equals or not
-     * */
-    private boolean assertArrayListExt(ArrayList<ArrayList<String>> expected,
-            ArrayList<ArrayList<String>> result){
-        for(ArrayList<String> listExp : expected){
-            boolean contains = false;
-            for(ArrayList<String> listRes : result){
-                if(listExp.containsAll(listRes) && listRes.containsAll(listExp)){
-                    contains = true;
-                    break;
-                }
-            }
-            if(!contains)    return false;
+    @Test
+    public void wordbreakII_edgeTest_01(){
+        String s = new String("aaaaab");
+        String[] arr = new String[]{"b", "a", "aa", "aaa", "aaaa", "aaaaa"};
+        Set<String> dict = new HashSet<String>();
+        fillCollection(arr, dict);
+        List<String> result = game.wordbreakall(s, dict);
+
+        String[] expectedArr = new String[]{"a a a a a b",
+                                            "a a a aa b",
+                                            "a a aa a b",
+                                            "a aa a a b",
+                                            "aa a a a b",
+                                            "a a aaa b",
+                                            "a aaa a b",
+                                            "aaa a a b",
+                                            "a aaaa b",
+                                            "aaaa a b",
+                                            "a aa aa b",
+                                            "aa a aa b",
+                                            "aa aa a b",
+                                            "aaa aa b",
+                                            "aa aaa b",
+                                            "aaaaa b"};
+        List<String> expected = new ArrayList<String>();
+        fillCollection(expectedArr, expected);
+        assertListExt(expected, result);
+    }
+
+    private void fillCollection(String[] arr, Collection<String> coll){
+        coll.clear();
+        for(String str : arr){
+            coll.add(str);
         }
+        return;
+    }
+    /*
+     * to assert two List<String> objects equals or not
+     * */
+    private boolean assertListExt(List<String> expected, List<String> result){
+        Assert.assertEquals(expected.size(), result.size());
+        Assert.assertTrue(expected.containsAll(result));
+        Assert.assertTrue(result.containsAll(expected));
         return true;
     }
 
