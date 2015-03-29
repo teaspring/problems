73788a81803f51a14053b31df5ae15ace6cfd323
teaspring <bruce.letter@gmail.com>
2015-03-29 23:14:56 +0800

    refactor anagrams.cpp, add unit test

---
+++ leetcode/cpp_unittest/anagrams_unittest/anagrams_unittest.cpp
@@ -0,0 +1,64 @@
+#include "anagrams.cpp"
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+/* vector<string> Solution::anagrams(vector<string>&) */
+
+/*
+ * assert two vector<> whether have same elements
+ * */
+bool comparestr(string str1, string str2){ // indicate whether 1st arg should go before 2nd arg
+    return strcmp(str1.c_str(), str2.c_str()) <= 0;
+}
+
+bool assertTwoStrVectors(vector<string>& expected, vector<string>& result){
+    EXPECT_EQ(expected.size(), result.size());
+
+    const int n = expected.size();
+    sort(expected.begin(), expected.end(), comparestr);
+    sort(result.begin(), result.end(), comparestr);
+
+    for(int i = 0; i < n; i++){
+        EXPECT_STREQ(expected[i].c_str(), result[i].c_str());
+    }
+    return true;
+}
+
+void fillStrVector(string arr[], int n, vector<string>& vect){
+    for(int i = 0; i < n; i++){
+        vect.push_back(string(arr[i]));
+    }
+}
+
+TEST(anagramsTest, Positive01){
+    Solution s;
+
+    vector<string> dict;
+    string arr[]{"dog", "god", "odg", "cat"};
+    fillStrVector(arr, 4, dict);
+
+    vector<string> expected;
+    string expArr[]{"dog", "odg", "god"};
+    fillStrVector(expArr, 3, expected);
+
+    vector<string> result = s.anagrams(dict);
+    EXPECT_TRUE(assertTwoStrVectors(expected, result));
+}
+
+TEST(anagramsTest, Positive02){
+    Solution s;
+
+    vector<string> dict;
+    string arr[]{"red", "god", "odg", "cat", "rde", "der", "dre"};
+    fillStrVector(arr, 7, dict);
+
+    vector<string> expected;
+    string expArr[]{"odg", "god", "red", "rde", "der", "dre"};
+    fillStrVector(expArr, 6, expected);
+
+    vector<string> result = s.anagrams(dict);
+    EXPECT_TRUE(assertTwoStrVectors(expected, result));
+}
--- leetcode/src/anagrams.cpp
+++ leetcode/src/anagrams.cpp
@@ -6,82 +6,62 @@
 #include <set>
 #include <map>
 
-typedef multimap<string, int>::iterator        iterType;
-typedef pair<string, int>                      pairType;
+typedef multimap<string, int>::iterator  iterType;
+typedef pair<string, int>  pairType;
 
-string wordkey(const string& word){        //count sort word, as its key
-    char *A = new char[26];
-    memset(A, 0, sizeof(char)*26);
-    int n = word.size();
-    for(int i=0;i<n;i++){
-        A[word[i] - 'a']++;
-    }
-    for(int i=1;i<26;i++){
-        A[i] += A[i-1];
-    }
-    char *B = new char[n+1];
-    for(int i=0;i<n;i++){
-        B[A[word[i] - 'a'] - 1] = word[i];
-        A[word[i] - 'a']--;
-    }
-    B[n] = '\0';
-    string str(B);
-    delete[] A;
-    delete[] B;
-    return str;
-}
+class Solution{
 
-vector<string> anagrams(vector<string>& strs){
-    vector<string> res;
-    int n = strs.size();
-    if(n==0)    return res;
-    multimap<string, int> mmp;  //pair(key of word, index in strs)
-    set<string>    keys;        //save key of word
-    for(int i=0;i<n;i++){
-        string kstr = wordkey(strs[i]);
-        keys.insert(kstr);
-        mmp.insert(pairType(kstr, i));
-    }
-    
-    for(set<string>::iterator kIter = keys.begin(); kIter != keys.end(); ++kIter){
-        if(mmp.count(*kIter) == 1)    continue;
-        pair<iterType, iterType> ret = mmp.equal_range(*kIter);   //(lowerbound, upperbound) multimap::equal_range()
-        for(iterType vIter = ret.first; vIter != ret.second; ++vIter){
-            res.push_back(strs[vIter->second]);
+public:
+    vector<string> anagrams(vector<string>& strs){
+        vector<string> res;
+        if(strs.empty())    return res;
+
+        const int n = strs.size();
+        multimap<string, int> mmp;  //pair(key of word, index in strs)
+        set<string>    keys;        //save key of word
+        for(int i = 0;i < n; i++){
+            string kstr = wordkey(strs[i]);
+            keys.insert(kstr);
+            mmp.insert(pairType(kstr, i));
         }
-    }
-    return res;
-}
+    
+        for(set<string>::iterator kIter = keys.begin(); kIter != keys.end(); ++kIter){
 
-void showVector(const vector<string>& vec){
-    int n = vec.size();
-    for(int i =0;i<n;i++){
-        printf("%s, ", vec[i].c_str());
-    }
-    printf("\n");
-}
+            if(mmp.count(*kIter) == 1)    continue;
 
-void test_01(){        //test wordkey()
-    string str;
-    while(1){
-        printf("please word to sort in char:\n");
-        if(getline(cin, str)==0 || str.empty())        break;
-        printf("sorted: %s\n", wordkey(str).c_str());
-    }
-}
+            pair<iterType, iterType> ret = mmp.equal_range(*kIter);   //(lowerbound, upperbound) multimap::equal_range()
 
-void test_02(){       //test anagrams()
-    string str;
-    while(1){
-        printf("please input word list:\n");
-        if(getline(cin, str)==0 || str.empty())        break;
-        vector<string> vec = splitStr2Vector(str);
-        vector<string> res = anagrams(vec);
-        showVector(res);
+            for(iterType vIter = ret.first; vIter != ret.second; ++vIter){
+                res.push_back( strs[vIter->second] );
+            }
+        }
+        return res;
     }
-}
 
-int main(int, char**){
-    test_02();
-    return 0;
-}
+    string wordkey(const string& word){ // count sort word, as its key
+        char A[26];
+        memset(A, 0, sizeof(char)*26);
+
+        const int n = word.size();
+        for(int i = 0; i < n; i++){
+            A[word[i] - 'a']++;
+        }
+
+        for(int i = 1;i < 26; i++){
+            A[i] += A[i-1];
+        }
+
+        char B[n+1];
+        memset(B, 0, sizeof(B) / sizeof(char));
+
+        for(int i = 0; i < n; i++){
+            int chIndex = word[i] - 'a';
+            B[ A[chIndex] - 1 ] = word[i];
+            A[chIndex]--;
+        }
+        B[n] = '\0';
+
+        return string(B);
+    }
+};
+
