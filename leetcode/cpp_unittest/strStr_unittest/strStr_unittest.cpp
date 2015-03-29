e0fc0006cdcb2c7ca235671e45ed3f7cee1f4f40
teaspring <bruce.letter@gmail.com>
2015-03-29 10:01:31 +0800

    unittest of strStr.cpp

---
+++ leetcode/cpp_unittest/strStr_unittest/strStr_unittest.cpp
@@ -0,0 +1,34 @@
+#include "strStr.cpp"
+#include "gtest/gtest.h"
+
+/* char* Solution::strStr(char *str, char *pattern) */
+
+TEST(strStrTest, Negative01){
+    Solution s;
+
+    const char *str = "abaaba";
+    const char *pattern = "ababa";
+
+    const char *expected = NULL;
+    EXPECT_EQ(expected, s.strStr(str, pattern));
+}
+
+TEST(strStrTest, Positive01){
+    Solution s;
+
+    const char *str = "abbaabbaabab";
+    const char *pattern = "abab";
+
+    const char *expected = "abab";
+    EXPECT_STREQ(expected, s.strStr(str, pattern));
+}
+
+TEST(strStrTest, Positive02){
+    Solution s;
+
+    const char *str = "aabaaaabaaaaa";
+    const char *pattern = "aaaaa";
+
+    const char *expected = "aaaaa";
+    EXPECT_STREQ(expected, s.strStr(str, pattern));
+}
