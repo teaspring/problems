fcc3be737c685e918a1136643790afe381007189
teaspring <bruce.letter@gmail.com>
2015-03-30 21:55:45 +0800

    refactor combinationsumII.cpp, add unit test case

--- leetcode/src/combinationsumII.cpp
+++ leetcode/src/combinationsumII.cpp
@@ -8,76 +8,46 @@
 #include "../include/preliminary.h"
 #include <algorithm>
 
-vector<int> index2Values(const vector<int>& candidates, vector<int>& indexes){
-    vector<int> values;
-    int n = indexes.size();
-    for(int i=0; i<n; ++i){
-        values.push_back(candidates[indexes[i]]);
-    }
-    return values;
-}
+class Solution{
 
-void plusSum(vector<vector<int> >& res, const vector<int>& candidates, vector<int>& vec, int target){
-    if(target==0){
-        res.push_back(index2Values(candidates, vec));
+public:
+    vector<vector<int> > combinationSumII(vector<int>& num, int target){
+        sort(num.begin(), num.end());  //sort in ascending order
+        vector<vector<int> > res;
+        vector<int> vec;
+        plusSum(res, num, vec, target);
+        return res;
+    }
+
+private:
+    vector<int> index2Values(const vector<int>& candidates, vector<int>& indexes){
+        vector<int> values;
+        int n = indexes.size();
+        for(int i = 0; i < n; ++i){
+            values.push_back(candidates[indexes[i]]);
+        }
+        return values;
+    }
+
+    void plusSum(vector<vector<int> >& res, const vector<int>& candidates, vector<int>& vec, int target){
+        if(target == 0){
+            res.push_back(index2Values(candidates, vec));
+            return;
+        }
+
+        int n = candidates.size();
+        int start = vec.empty() ? 0 : vec[vec.size()-1] + 1;
+        for(int i = start; i < n; ++i){
+            if(target < candidates[i])        break;
+
+            if(i > start &&  candidates[i] == candidates[i-1])    continue;  // skip duplicat combinations
+
+            vec.push_back(i);
+            plusSum(res, candidates, vec, target - candidates[i]);
+            vec.pop_back();
+        }
         return;
     }
-    int n = candidates.size();
-    int last = vec.empty() ? 0 : vec[vec.size()-1] + 1;
-    for(int i=last; i<n; ++i){
-        if(target < candidates[i])        break;
-        if(i>last &&  candidates[i] == candidates[last])    continue;  //key to skip duplicat combinations
-        vec.push_back(i);
-        plusSum(res, candidates, vec, target - candidates[i]);
-        vec.pop_back();
-        last=i;
-    }
-    return;
-}
 
-vector<vector<int> > combinationSum2(vector<int>& num, int target){
-    sort(num.begin(), num.end());  //sort in ascending order 
-    vector<vector<int> > res;
-    vector<int> vec;
-    plusSum(res, num, vec, target);
-    return res;
-}
-
-/**********************test************************/
-void displayVecVec(const vector<vector<int> >& combinations){
-    int n = combinations.size();
-    for(int i=0; i<n; ++i){
-        int m = combinations[i].size();
-        printf("[");
-        if(m>0)        printf("%d", combinations[i][0]);
-        for(int j=1; j<m; ++j){
-            printf(", %d", combinations[i][j]);
-        }
-        printf("]\n");
-    }
-}
-
-void test_01(){
-    string str;
-    while(1){
-        printf("please input positive integer candidates:\n");
-        if(getline(cin, str)==0 || str.empty())        break;
-        int *arr = new int[str.size()]();
-        int n = splitStr2IntArray(str, arr);
-        vector<int> candi;
-        for(int i=0;i<n;++i){
-            candi.push_back(arr[i]);
-        }
-
-        printf("please input target integer:\n");
-        if(getline(cin, str)==0 || str.empty())        break;
-        int t = atoi(str.c_str());
-        vector<vector<int> > res = combinationSum2(candi, t);
-        displayVecVec(res);
-    }
-}
-
-int main(int, char**){
-    test_01();
-    return 0;
-}
+    /* unit test is in ../cpp_unittest/combinationsumII_unittest */
+};
