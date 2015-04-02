70e766cbe623c85771fa049c8234dd911d5fa40d
teaspring <teaspring@github.com>
2015-04-02 09:16:39 +0800

    refactir gasstation.cpp, add unit test

---
+++ leetcode/cpp_unittest/gasstation_unittest/gasstation_unittest.cpp
@@ -0,0 +1,56 @@
+#include "gasstation.cpp"
+#include "gtest/gtest.h"
+
+/* int Solution::gasstation(vector<int>& gas, vector<int>& cost) */
+
+TEST(gasstationTest, Positive01){
+    Solution s;
+
+    int gasArr[]  = {1, 2, 3, 4, 5};
+    vector<int> gas(gasArr, gasArr + sizeof(gasArr)/sizeof(int));
+    int costArr[] = {3, 4, 5, 1, 2};
+    vector<int> cost(costArr, costArr + sizeof(costArr)/sizeof(int));
+
+    int expected = 3;
+
+    EXPECT_EQ(expected, s.gasstation(gas, cost));
+}
+
+TEST(gasstationTest, Positive02){
+    Solution s;
+
+    int gasArr[]  = {1, 2};
+    vector<int> gas(gasArr, gasArr + sizeof(gasArr)/sizeof(int));
+    int costArr[] = {2, 1};
+    vector<int> cost(costArr, costArr + sizeof(costArr)/sizeof(int));
+
+    int expected = 1;
+
+    EXPECT_EQ(expected, s.gasstation(gas, cost));
+}
+
+TEST(gasstationTest, Negative01){
+    Solution s;
+
+    int gasArr[]  = {1};
+    vector<int> gas(gasArr, gasArr + sizeof(gasArr)/sizeof(int));
+    int costArr[] = {2};
+    vector<int> cost(costArr, costArr + sizeof(costArr)/sizeof(int));
+
+    int expected = -1;
+
+    EXPECT_EQ(expected, s.gasstation(gas, cost));
+}
+
+TEST(gasstationTest, Negative02){
+    Solution s;
+
+    int gasArr[]  = {1, 2, 3};
+    vector<int> gas(gasArr, gasArr + sizeof(gasArr)/sizeof(int));
+    int costArr[] = {3, 2, 3};
+    vector<int> cost(costArr, costArr + sizeof(costArr)/sizeof(int));
+
+    int expected = -1;
+
+    EXPECT_EQ(expected, s.gasstation(gas, cost));
+}
