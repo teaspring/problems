a0a6740fef86b62531c31b0921d7cbecf7a16d3f
teaspring <bruce.letter@gmail.com>
2015-04-03 21:35:44 +0800

    add unit test of max2DRectangle.cpp

---
+++ leetcode/cpp_unittest/max2DRectangle_unittest/max2DRectangle_unittest.cpp
@@ -0,0 +1,57 @@
+#include "max2DRectangle.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * int Solution::maximumRectangle(vector<vector<char> >&)
+ *
+ * int Solution::largestRectangleArea(vector<int>&)
+ * */
+
+TEST(largestRectangleAreaTest, Positive01){
+    Solution s;
+
+    int arr[] = {1, 2, 2, 3, 2};
+    vector<int> heights(arr, arr + sizeof(arr)/sizeof(int));
+
+    int expected = 8;
+    EXPECT_EQ(expected, s.largestRectangleArea(heights));
+}
+
+TEST(largestRectangleAreaTest, Positive02){
+    Solution s;
+
+    int arr[] = {1, 2, 1, 4, 3};
+    vector<int> heights(arr, arr + sizeof(arr)/sizeof(int));
+
+    int expected = 6;
+    EXPECT_EQ(expected, s.largestRectangleArea(heights));
+}
+
+TEST(maximumRectangleTest, Positive01){
+    Solution s;
+
+    char arr[][5] = {{'1', '0', '1', '1', '1'},
+                     {'0', '1', '1', '1', '0'},
+                     {'1', '0', '1', '1', '1'}};
+    vector<vector<char> > matrix;
+    for(int i = 0; i < 3; i++){
+        matrix.push_back(vector<char>(arr[i], arr[i] + 5));
+    }
+
+    int expected = 6;
+    EXPECT_EQ(expected, s.maximumRectangle(matrix));
+}
+
+TEST(maximumRectangleTest, Negative01){
+    Solution s;
+
+    char arr[][2] = {{'1', '0'},
+                     {'0', '1'}};
+    vector<vector<char> > matrix;
+    for(int i = 0; i < 2; i++){
+        matrix.push_back(vector<char>(arr[i], arr[i] + 2));
+    }
+
+    int expected = 1;
+    EXPECT_EQ(expected, s.maximumRectangle(matrix));
+}
--- leetcode/src/max2DRectangle.cpp
+++ leetcode/src/max2DRectangle.cpp
@@ -1,20 +1,26 @@
 /*
  * a matrix is made up of '1' and '0', find the maximum rectangle which ONLY contains '1'
+ *
+ * [1, 2, 4, 0, 2], every int has one unit of width. max area is 4
  */
 
 #include "../include/preliminary.h"
 
-class Max2DRectangle{
+class Solution{
 public:
     int maximumRectangle(vector<vector<char> >& matrix){ //to project 2D matrix to 1D array eventually
         if(matrix.empty())    return 0;
-        int m = matrix.size(), n = matrix[0].size(), res = 0;
-        if(n==0)    return 0;
+
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        int res = 0;
+        if(n == 0)    return 0;
+
         int horizon[n];
         memset(horizon, 0, sizeof(horizon));
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i = 0; i < m; i++){ // each row
+            for(int j = 0; j < n; j++){
                 if(matrix[i][j] == '1'){
                     horizon[j] += 1;
                 }else{
@@ -24,49 +30,35 @@ public:
             vector<int> height(horizon, horizon + n);
             int tmp = largestRectangleArea(height);
             res = max(res, tmp);
+
+            height.clear();
         }
         return res;
     }
 
-    int largestRectangleArea(const vector<int>& height){ //max rectangle in one array, time is O(n)
-        int n = height.size(), res = 0;
-        if(n == 0)    return res;
-        stack<int> stk;    // store index
-        for(int i=0; i<=n; i++){  // include i==n avoids the appendix process
-            int h = i == n ? 0 : height[i];
+    int largestRectangleArea(const vector<int>& heights){ //max rectangle in one array, time is O(n)
+        const int n = heights.size();
+        if(n == 0)    return 0;
+
+        int res = 0;
+        stack<int> stk; // store index
+
+        for(int i = 0; i <= n; i++){  // include i == n avoids the appendix process
+            int h = (i == n ? 0 : heights[i]);
             while(!stk.empty()){
                 int p = stk.top();
-                if(height[p] < h)    break;
+                if(heights[p] < h)    break;
+
                 stk.pop();
-                int start = stk.empty() ? -1 : stk.top();  // start is exclusive index in left
-                res = max(res, (i-1 - start) * height[p]); // i-1 is inclusive index
+                int start = (stk.empty() ? -1 : stk.top());  // start is exclusive index in left
+
+                int tmp = (i-1 - start) * heights[p]; // i-1 is inclusive index
+                res = max(res, tmp);
             }
-            stk.push(i);
+            stk.push(i); // push greater height
         }
         return res;
     }
-
-    void test_01(){
-        vector<vector<char> > matrix;
-        char arr0[] = {'1', '0'};
-        matrix.push_back(vector<char>(arr0, arr0 + sizeof(arr0)/sizeof(char)));
-        char arr1[] = {'0', '1'};
-        matrix.push_back(vector<char>(arr1, arr1 + sizeof(arr1)/sizeof(char)));
-        cout << maximumRectangle(matrix) << endl;
-    }
-
-    void test_02(){
-        vector<vector<char> > matrix;
-        char arr0[] = {'1'};
-        matrix.push_back(vector<char>(arr0, arr0 + sizeof(arr0)/sizeof(char)));
-        cout << maximumRectangle(matrix) << endl;
-    }
-
 };
 
-int main(){
-    Max2DRectangle m2d;
-    m2d.test_01();
-    m2d.test_02();
-    return 0;
-}
+/* unit test is in ../cpp_unittest/max2DRectangle_unittest */
