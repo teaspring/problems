e4164b66fa46b90d35586069d36f978e1c20f216
teaspring <bruce.letter@gmail.com>
2015-04-07 21:40:06 +0800

    refactor partitionpalindrome.cpp, add unit test

---
+++ leetcode/cpp_unittest/partitionpalindrome_unittest/partitionpalindrome_unittest.cpp
@@ -0,0 +1,111 @@
+/*
+ * vector<vector<string> > Solution::partition(const string&)
+ * */
+
+#include "partitionpalindrome.cpp"
+#include "gtest/gtest.h"
+#include <algorithm>
+
+/* customized methods to falicitate construct and assert */
+
+bool equalsList(vector<string>& list1, vector<string>& list2){
+    if(list1.size() != list2.size())    return false;
+
+    const int n = list1.size();
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+
+    for(int i = 0; i < n; i++){
+        if(strcmp(list1[i].c_str(), list2[i].c_str()) != 0)
+          return false;
+    }
+    return true;
+}
+
+bool containsVector(vector<vector<string> >& duoList, vector<string>& list){
+    const int n = duoList.size();
+    for(int i = 0; i < n; i++){
+        if(equalsList(duoList[i], list))    return true;
+    }
+    return false;
+}
+
+bool equalsDuoVector(vector<vector<string> >& duoList1, vector<vector<string> >& duoList2){
+    EXPECT_EQ(duoList1.size(), duoList2.size());
+
+    const int n = duoList1.size();
+    for(int i = 0; i < n; i++){
+        EXPECT_TRUE(containsVector(duoList1, duoList2[i]));
+        EXPECT_TRUE(containsVector(duoList2, duoList1[i]));
+    }
+    return true;
+}
+
+/*---------------------- test case ----------------------*/
+
+TEST(partitionpalindromeTest, Positive01){
+    Solution s;
+
+    string str("abba");
+
+    string arr1[] = {"a", "b", "b", "a"};
+    string arr2[] = {"abba"};
+    string arr3[] = {"a", "bb", "a"};
+
+    vector<vector<string> > expected;
+    expected.push_back(vector<string>(arr1, arr1 + 4));
+    expected.push_back(vector<string>(arr2, arr2 + 1));
+    expected.push_back(vector<string>(arr3, arr3 + 3));
+
+    vector<vector<string> > result = s.partition(str);
+
+    EXPECT_TRUE(equalsDuoVector(expected, result));
+}
+
+TEST(partitionpalindromeTest, Positive02){
+    Solution s;
+
+    string str("abbaa");
+
+    string arr1[] = {"a", "b", "b", "a", "a"};
+    string arr2[] = {"abba", "a"};
+    string arr3[] = {"a", "bb", "a", "a"};
+    string arr4[] = {"a", "bb", "aa"};
+    string arr5[] = {"a", "b", "b", "aa"};
+
+    vector<vector<string> > expected;
+    expected.push_back(vector<string>(arr1, arr1 + 5));
+    expected.push_back(vector<string>(arr2, arr2 + 2));
+    expected.push_back(vector<string>(arr3, arr3 + 4));
+    expected.push_back(vector<string>(arr4, arr4 + 3));
+    expected.push_back(vector<string>(arr5, arr5 + 4));
+
+    vector<vector<string> > result = s.partition(str);
+
+    EXPECT_TRUE(equalsDuoVector(expected, result));
+}
+
+TEST(partitionpalindromeTest, Positive03){
+    Solution s;
+
+    string str("abbaab");
+
+    string arr1[] = {"a", "b", "b", "a", "a", "b"};
+    string arr2[] = {"abba", "a", "b"};
+    string arr3[] = {"a", "b", "baab"};
+    string arr4[] = {"a", "bb", "a", "a", "b"};
+    string arr5[] = {"a", "bb", "aa", "b"};
+    string arr6[] = {"a", "b", "b", "aa", "b"};
+
+    vector<vector<string> > expected;
+    expected.push_back(vector<string>(arr1, arr1 + 6));
+    expected.push_back(vector<string>(arr2, arr2 + 3));
+    expected.push_back(vector<string>(arr3, arr3 + 3));
+    expected.push_back(vector<string>(arr4, arr4 + 5));
+    expected.push_back(vector<string>(arr5, arr5 + 4));
+    expected.push_back(vector<string>(arr6, arr6 + 5));
+
+    vector<vector<string> > result = s.partition(str);
+
+    EXPECT_TRUE(equalsDuoVector(expected, result));
+}
--- leetcode/src/partitionpalindrome.cpp
+++ leetcode/src/partitionpalindrome.cpp
@@ -16,108 +16,93 @@
 
 using namespace std;
 
-/*
- * make substring palindrome vector<>, via dynamic programming, in time O(n^2)
- * */
-void make_vpalin(const string& s, vector<vector<int> >& vpalin){
-    int n = s.size();
-    int **dp = new int*[n];
-    for(int i=0;i<n;i++){
-        dp[i] = new int[n+1]();    //length is [0,n]!!
+class Solution{
+
+public:
+    vector<vector<string> > partition(const string& s){
+        vector<vector<string> > res;
+        const int n = s.size();
+        if(n == 0)    return res;
+        if(n == 1){
+            vector<string> vec;
+            vec.push_back(s);
+            res.push_back(vec);
+            return res;
+        }
+
+        vector<vector<int> > vpalin;
+        for(int i = 0; i < n; i++){
+            vector<int> vec;
+            vpalin.push_back(vec);
+        }
+
+        make_vpalin(s, vpalin);
+
+        vector<int> vec;
+        vec.push_back(0);
+        partition_palin(res, vpalin, s, 0, vec);
+        vec.pop_back();
+
+        return res;
     }
-    for(int i=0;i<n;i++){
-        dp[i][1] = 1;
-        vpalin[i].push_back(i+1);    //vpalin[i] has one element (i+1) at least, so nothing can be skipped
-    }
-    for(int l=2;l<=n;l++){
-        for(int end=n;end-l >= 0;end--){
-            if(l<4 || dp[end-l+1][l-2]==1){
-                if(s[end-l] == s[end-1]){
-                    dp[end-l][l] = 1;
-                    vpalin[end-l].push_back(end);
+
+private:
+    /*
+    * make substring palindrome vector<>, via dynamic programming, in time O(n^2)
+    * */
+    void make_vpalin(const string& s, vector<vector<int> >& vpalin){
+        const int n = s.size();
+
+        int dp[n][n+1]; // dp[][] tells whether s.substr() is palindrome or not
+        for(int i = 0; i < n; i++){
+            memset(dp[i], 0, sizeof(dp[i]));
+        }
+
+        for(int i = 0; i < n; i++){
+            dp[i][1] = 1;
+            vpalin[i].push_back(i+1); // vpalin[i] tells for substr starting at [i], which is palindrome
+        }
+
+        for(int l = 2; l <= n; l++){
+            for(int end = n; end-l >= 0; end--){
+                if(l < 4 || dp[end - l + 1][l-2] == 1){ // dp is necessary since vector<> not support contains()
+                    if(s[end-l] == s[end-1]){
+                        dp[end-l][l] = 1;
+                        vpalin[end-l].push_back(end);
+                    }
                 }
             }
         }
-    }
-    for(int i=0;i<n;i++){
-        delete[] dp[i];
-        dp[i] = 0;
-    }
-    delete[] dp;
-    dp=0;
-    return;
-}
 
-/*
- * make up the partition string
- * */
-void partition_palin(vector<vector<string> >& res, const vector<vector<int> >& vpalin, const string& s, int start, stack<int> stk){
-    if(start==(int)s.size()){
-        stack<int> stk2;
-        while(!stk.empty()){
-            stk2.push(stk.top());
-            stk.pop();
-        }
-        vector<string> vstr;
-        int start = stk2.top();    //it must be 0
-        stk.push(start);
-        stk2.pop();
-        while(!stk2.empty()){
-            vstr.push_back(s.substr(start, stk2.top()-start));
-            start = stk2.top();
-            stk.push(start);
-            stk2.pop();
-        }
-        res.push_back(vstr);
         return;
     }
 
-    vector<int> vec = vpalin[start];
-    for(size_t i=0;i<vec.size();i++){
-        stk.push(vec[i]);
-        partition_palin(res, vpalin, s, vec[i], stk);
-        stk.pop();
-    }
-    return;
-}
-
-vector<vector<string> > partition(const string& s){
-    vector<vector<string> > res;
-    int n = s.size();
-    if(n==0)    return res;
-    if(n==1){
-        vector<string> vec;
-        vec.push_back(s);
-        res.push_back(vec);
-        return res;
-    }
-    vector<vector<int> > vpalin;
-    for(int i=0;i<n;i++){
-        vector<int> vec;
-        vpalin.push_back(vec);
-    }
-    make_vpalin(s, vpalin);
-    stack<int> stk;
-    stk.push(0);
-    partition_palin(res, vpalin, s, 0, stk);
-    stk.pop();
-    return res;
-}
-
-int main(int, char**){
-    string str;
-    while(1){
-        cout<<"please input string:"<<endl;
-        if(getline(cin, str)==0 || str.empty())        break;
-        vector<vector<string> > res = partition(str);
-        for(size_t i=0;i<res.size();i++){
-            vector<string> vec = res[i];
-            for(size_t j=0;j<vec.size();j++){
-                printf("%s ", vec[j].c_str());
+    /*
+    * make up the partition string
+    * */
+    void partition_palin(vector<vector<string> >& res,
+                         const vector<vector<int> >& vpalin,
+                         const string& s,
+                         int start,
+                         vector<int>& idxs){
+        if(start == (int) s.size()){ // reach end of s, time to push result
+            vector<string> vstr;
+            const int m = idxs.size();
+            for(int i = 0; i < m-1; i++){
+                vstr.push_back(s.substr(idxs[i], idxs[i+1] - idxs[i]));
             }
-            printf("\n");
+            res.push_back(vstr);
+            return;
         }
-    }
-    return 0;
-}
 
+        vector<int> vec = vpalin[start]; // pay extra structure for readability
+        const int n = vec.size();
+        for(int i = 0; i < n; i++){
+            idxs.push_back(vec[i]);
+            partition_palin(res, vpalin, s, vec[i], idxs);
+            idxs.pop_back();
+        }
+
+        return;
+    }
+};
