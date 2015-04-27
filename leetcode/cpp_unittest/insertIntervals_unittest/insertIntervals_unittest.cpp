2bed0e769d539789ce2734c5ce3eee5751f8170a
teaspring <bruce.letter@gmail.com>
2015-04-27 16:10:39 +0800

    refactor insertIntervals.cpp, add unit test

---
+++ leetcode/cpp_unittest/insertIntervals_unittest/insertIntervals_unittest.cpp
@@ -0,0 +1,78 @@
+#include "insertIntervals.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * vector<Interval> insert(vector<Interval>& intervals, Interval newElement)
+ * */
+
+vector<Interval> constructVector(int *A, int *B, int n){
+    vector<Interval> vec;
+    for(int i = 0; i < n; i++){
+        vec.push_back(Interval(A[i], B[i]));
+    }
+    return vec;
+}
+
+void assertIntervals(const vector<Interval>& expected, const vector<Interval>& result){
+    EXPECT_EQ(expected.size(), result.size());
+
+    const int n = expected.size();
+    for(int i = 0; i < n; i++){
+        EXPECT_EQ(expected[i].start, result[i].start);
+        EXPECT_EQ(expected[i].end, result[i].end);
+    }
+}
+
+// insert intermedia position
+TEST(insertIntervalsTest, Positive01){
+    Solution s;
+
+    int A[] = {1, 4, 6};
+    int B[] = {2, 5, 7};
+    vector<Interval> intervals = constructVector(A, B, sizeof(A)/sizeof(int));
+
+    Interval nElement(3, 4);
+
+    int C[] = {1, 3, 4, 6};
+    int D[] = {2, 4, 5, 7};
+    vector<Interval> expected = constructVector(C, D, sizeof(C)/sizeof(int));
+
+    vector<Interval> result = s.insert(intervals, nElement);
+    assertIntervals(expected, result);
+}
+
+// insert after tail
+TEST(insertIntervalsTest, Positive02){
+    Solution s;
+
+    int A[] = {1, 4, 6};
+    int B[] = {2, 5, 7};
+    vector<Interval> intervals = constructVector(A, B, sizeof(A)/sizeof(int));
+
+    Interval nElement(7, 9);
+
+    int C[] = {1, 4, 6, 7};
+    int D[] = {2, 5, 7, 9};
+    vector<Interval> expected = constructVector(C, D, sizeof(C)/sizeof(int));
+
+    vector<Interval> result = s.insert(intervals, nElement);
+    assertIntervals(expected, result);
+}
+
+// insert in front of head
+TEST(insertIntervalsTest, Positive03){
+    Solution s;
+
+    int A[] = {1, 4, 6};
+    int B[] = {2, 5, 7};
+    vector<Interval> intervals = constructVector(A, B, sizeof(A)/sizeof(int));
+
+    Interval nElement(0, 1);
+
+    int C[] = {0, 1, 4, 6};
+    int D[] = {1, 2, 5, 7};
+    vector<Interval> expected = constructVector(C, D, sizeof(C)/sizeof(int));
+
+    vector<Interval> result = s.insert(intervals, nElement);
+    assertIntervals(expected, result);
+}
--- leetcode/src/insertIntervals.cpp
+++ leetcode/src/insertIntervals.cpp
@@ -2,92 +2,63 @@
  * given a set of non-overlapping intervals, insert a new interval into the intervals. 
  * You can assume that the intervals were initially sorted according to their start time.
  *
- * 
  * */
 
-#include "../include/preliminary.h"
+#include <vector>
+
+using namespace std;
 
 struct Interval{
     int start;
     int end;
-    Interval():start(0), end(0){}
-    Interval(int s, int e):start(s), end(e){} 
+    Interval(): start(0), end(0){}
+    Interval(int s, int e): start(s), end(e){}
 };
 
-void displayIntervals(const vector<Interval>& intervals){
-    for(vector<Interval>::const_iterator iter = intervals.begin();
-        iter < intervals.end();
-        ++iter){
-        printf("[%d, %d] ", iter->start, iter->end);
-    }
-    printf("\n");
-}
+class Solution{
 
-/*
- * linear solution, time in O(n)
- * */
-vector<Interval> insert(vector<Interval>& intervals, Interval newInterval){
-    vector<Interval> res;
-    bool inserted = false;
-    for(vector<Interval>::const_iterator iter = intervals.begin();
-        iter != intervals.end();
-        ++iter){
-        if(!inserted){
-            bool overlapped = (iter->end >= newInterval.start) && (newInterval.end >= iter->start);
-            if(overlapped){
-                int a = min(newInterval.start, iter->start);
-                int b = max(newInterval.end, iter->end);
-                res.push_back(Interval(a, b));
-                inserted = true;
-            }else{
-                if(newInterval.end < iter->start){
-                    res.push_back(Interval(newInterval.start, newInterval.end));
-                    inserted = true;
-                }
-                res.push_back(Interval(iter->start, iter->end));
+public:
+    /*
+     * vector::insert(iteration position, const value_type& val) # insert new element before the one at position
+     * find the position via binary search, then call vector::insert()
+     * time O(lgn)
+     * */
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval){
+        vector<Interval>::iterator v = intervals.begin(), u = intervals.end(); // v is inclusive, u is exclusive
+        while(v < u){
+            vector<Interval>::iterator m = v + (u - v) / 2;
+
+            if(compareInterval(newInterval, *m) < 0
+            && (m == intervals.begin() || compareInterval(*(m-1), newInterval) < 0)){ // find the position
+                intervals.insert(m, newInterval);
+                return  intervals;
             }
+
+            if(compareInterval(newInterval, *m) > 0){
+                v = m+1;
+            }else{
+                u = m;
+            }
+        }
+
+        // now v == u
+        if(v == intervals.end()){
+            intervals.push_back(newInterval);
         }else{
-            int m = res.size();
-            if(res[m-1].end < iter->start){
-                res.push_back(Interval(iter->start, iter->end));
-            }else{
-                res[m-1].end = max(res[m-1].end, iter->end);
-            }
+            intervals.insert(v, newInterval);
         }
-    }
-    if(!inserted){    //don't forget insert it if not yet
-        res.push_back(Interval(newInterval.start, newInterval.end));
-    }
-    return res;
-}
 
-void test_01(){
-    string str;
-    while(1){
-        cout << "please input integer start/end of intervals in pairs:" << endl;
-        if(getline(cin, str)==0 || str.empty())        break;
-        int *arr = new int[str.size()]();
-        int n = splitStr2IntArray(str, arr);
-        vector<Interval> intervals;
-        for(int i=0; i < (n>>1); ++i){
-            intervals.push_back(Interval(arr[i<<1], arr[(i<<1) + 1]));
-        }
-        delete[] arr;
-        arr=0;
-
-        cout << "please input new Interval to insert:" << endl;
-        if(getline(cin, str)==0 || str.empty())        continue;
-        arr = new int[str.size()]();
-        n = splitStr2IntArray(str, arr);
-        if(n < 2)        continue;
-        Interval added(arr[0], arr[1]);
-        
-        displayIntervals(insert(intervals, added));
+        return  intervals;
     }
-    return;
-}
 
-int main(int, char**){
-    test_01();
-    return 0;
-}
+private:
+    /*
+     * as the intervals have no overlap, compare their start is OK
+     * */
+    int compareInterval(const Interval& l, const Interval& r){
+        if(l.start == r.start)    return 0;
+        return l.start < r.start ? -1 : 1;
+    }
+};
+
+/* unit test is in ../cpp_unittest/insertIntervals_unittest */
