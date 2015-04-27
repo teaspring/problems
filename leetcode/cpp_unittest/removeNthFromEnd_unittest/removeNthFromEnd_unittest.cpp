d9d57bcc809dbb2a3c90abc275f09da2a9c6e733
teaspring <bruce.letter@gmail.com>
2015-04-27 09:16:54 +0800

    refactor removeNthFromEnd.cpp, add unit test while update Makefile

---
+++ leetcode/cpp_unittest/removeNthFromEnd_unittest/removeNthFromEnd_unittest.cpp
@@ -0,0 +1,94 @@
+#include "removeNthFromEnd.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * ListNode* removeNthFromEnd(ListNode* head, int n)
+ *
+ * in preliminary.cpp:
+ * struct ListNode
+ * ListNode* createSLL(int A[], int n)
+ * delSLL(ListNode *head)
+ *
+ * */
+
+void assertSLL(ListNode* head, int A[], int n){
+    ListNode *h = head;
+    for(int i = 0; i < n; i++){
+        EXPECT_EQ(A[i], h->val);
+        h = h->next;
+    }
+    EXPECT_EQ(NULL, h);
+}
+
+TEST(removeNthFromEndTest, Positive01){
+    Solution s;
+
+    int arr[] = {1, 2, 4, 3, 5};
+    ListNode* head = createSLL(arr, sizeof(arr)/sizeof(int));
+
+    const int k = 4;
+    int expected[] = {1, 4, 3, 5};
+
+    head = s.removeNthFromEnd(head, k);
+    assertSLL(head, expected, sizeof(expected)/sizeof(int));
+
+    delSLL(head);
+}
+
+TEST(removeNthFromEndTest, Positive02){
+    Solution s;
+
+    int arr[] = {0, 1, 2};
+    ListNode* head = createSLL(arr, sizeof(arr)/sizeof(int));
+
+    const int k = 2;
+    int expected[] = {0, 2};
+
+    head = s.removeNthFromEnd(head, k);
+    assertSLL(head, expected, sizeof(expected)/sizeof(int));
+
+    delSLL(head);
+}
+
+TEST(removeNthFromEndTest, Positive03){
+    Solution s;
+
+    int arr[] = {0, 1};
+    ListNode* head = createSLL(arr, sizeof(arr)/sizeof(int));
+
+    const int k = 1;
+    int expected[] = {0};
+
+    head = s.removeNthFromEnd(head, k);
+    assertSLL(head, expected, sizeof(expected)/sizeof(int));
+
+    delSLL(head);
+}
+
+TEST(removeNthFromEndTest, Positive04){
+    Solution s;
+
+    int arr[] = {0, 1};
+    ListNode* head = createSLL(arr, sizeof(arr)/sizeof(int));
+
+    const int k = 2;
+    int expected[] = {1};
+
+    head = s.removeNthFromEnd(head, k);
+    assertSLL(head, expected, sizeof(expected)/sizeof(int));
+
+    delSLL(head);
+}
+
+TEST(removeNthFromEndTest, Positive05){
+    Solution s;
+
+    int arr[] = {1};
+    ListNode* head = createSLL(arr, sizeof(arr)/sizeof(int));
+
+    const int k = 1;
+    head = s.removeNthFromEnd(head, k);
+    EXPECT_EQ(NULL, head);
+
+    delSLL(head);
+}
--- leetcode/src/removeNthFromEnd.cpp
+++ leetcode/src/removeNthFromEnd.cpp
@@ -2,93 +2,48 @@
  * given a SLL, remove the Nth node from the end of list and return its head.
  *
  * test data:
- * 1->2->3->4->5, n=2, after the removal, the SLL becomes 1->2->3->5
- * 1, 1,
- * 1->2, 2
- * 1->2, 1
+ * 1->2->3->4->5, n=2, return head of SLL 1->2->3->5
+ * 1, n=1,
+ * 1->2, n=2
+ * 1->2, n=1
  * */
 
 #include "../include/preliminary.h"
 
-ListNode* removeNth2End(ListNode *head, int n){
-    if(!head)    return NULL;
-    if(n==1){    //n==1 must be processed specially
-        ListNode *pr = NULL, *l = head;
-        while(l->next != NULL){
-            pr = l;
-            l = l->next;
+class Solution{
+
+public:
+    /*
+     * move p1 and p2 together where p2 of Nth back from p1
+     * */
+    ListNode* removeNthFromEnd(ListNode *head, int n){
+        if(!head)    return NULL;
+
+        ListNode *p1 = head; // p1 is the leading node
+        for(int i = 1; i < n && p1 != NULL; i++){ // n > 1 so p1 and p2 cannot be same node
+            p1 = p1->next;
         }
-        if(pr)    pr->next = NULL;
-        delete l;
-        return !pr ? NULL : head;
+
+        if(!p1)    return NULL; // out of range
+
+        ListNode *p2 = NULL, *prev = NULL;  // p2 is the Nth from p1, prev is pre of p2
+        while(p1 != NULL){  // when p1 becomes NULL, p2 is the one to delete
+            p1 = p1->next;
+            prev = p2;
+            p2 = ( !p2 ? head : p2->next);
+        }
+
+        if(prev != NULL){
+            prev->next = p2->next;
+        }else{ // prev is NULL, head is the one to delete
+            head = p2->next;
+        }
+
+        p2->next = NULL;
+        delete p2;
+
+        return head;
     }
+};
 
-    ListNode *p1 = head;
-    for(int i=1;i<n && p1 != NULL;i++){ //n>1 so p1 and p2 cannot be same node
-        p1 = p1->next;
-    }
-    if(!p1)        return NULL;
-    ListNode *p2 = head, *prev = NULL;
-    while(p1->next != NULL){
-        prev = p2;
-        p2 = p2->next;    //p2 is Nth node ahead of p1
-        p1 = p1->next;
-    }
-    ListNode *h = head;    //define another node pointer instead
-    if(prev != NULL){
-        prev->next = p2->next;
-    }else{
-        h = p2->next;
-    }
-    p2->next = NULL;
-    delete p2;
-    return h;
-}
-
-void test_01(){
-    ListNode *p0 = new ListNode(0);
-    ListNode *p1 = new ListNode(1);
-    p0->next = p1;
-    ListNode *p2 = new ListNode(2);
-    p1->next = p2;
-    displaySLL(p0);
-    ListNode *h = removeNth2End(p0, 2);
-    displaySLL(h);
-    delSLL(h);
-}
-
-void test_02(){
-    ListNode *p0 = new ListNode(0);
-    ListNode *p1 = new ListNode(1);
-    p0->next = p1;
-    displaySLL(p0);
-    ListNode *h = removeNth2End(p0, 1);
-    displaySLL(h);
-    delSLL(h);
-}
-
-void test_03(){
-    ListNode *p0 = new ListNode(0);
-    ListNode *p1 = new ListNode(1);
-    p0->next = p1;
-    displaySLL(p0);
-    ListNode *h = removeNth2End(p0, 2);
-    displaySLL(h);
-    delSLL(h);
-}
-
-void test_04(){
-    ListNode *p0 = new ListNode(0);
-    displaySLL(p0);
-    ListNode *h = removeNth2End(p0, 1);
-    displaySLL(h);
-    delSLL(h);
-}
-
-int main(int, char**){
-    test_01();
-    test_02();
-    test_03();
-    test_04();
-    return 0;
-}
+/* unit test is in ../cpp_unittest/removeNthFromEnd_unittest */
