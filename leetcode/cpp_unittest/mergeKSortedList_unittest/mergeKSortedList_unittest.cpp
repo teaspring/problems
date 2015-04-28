5b923e6d7b189ea7c8939cb9c6d1c77e9692ca92
teaspring <bruce.letter@gmail.com>
2015-04-29 07:42:05 +0800

    refactor mergeKSortedLists.cpp, add unit test

---
+++ leetcode/cpp_unittest/mergeKSortedList_unittest/mergeKSortedList_unittest.cpp
@@ -0,0 +1,69 @@
+#include "mergeKSortedList.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * ListNode* mergeKList(vector<ListNode*>&)
+ * */
+
+void assertList(ListNode *expected, ListNode *result){
+    ListNode *h1 = expected, *h2 = result;
+
+    while(h1 && h2){
+        EXPECT_EQ(h1->val, h2->val);
+        h1 = h1->next;
+        h2 = h2->next;
+    }
+
+    EXPECT_EQ(NULL, h1);
+    EXPECT_EQ(NULL, h2);
+}
+
+TEST(mergeKSortedListTest, Positive01){
+    Solution s;
+
+    const int rows = 5, cols = 4;
+    int arr[][cols] = {
+        {1, 5, 10, 12},
+        {2, 3, 11, 21},
+        {0, 4, 6, 20},
+        {-1, 7, 8, 19},
+        {6, 9, 14, 17}};
+
+    vector<ListNode*> lists;
+    vector<int> expectedVector;  // values of expected list
+
+    for(int i = 0; i < rows; i++){
+        ListNode *head = NULL, *h = NULL;
+        for(int j = 0; j < cols; j++){
+            int x = arr[i][j];
+            if(!head){
+                head = new ListNode(x);
+                h = head;
+            }else{
+                h->next = new ListNode(x);
+                h = h->next;
+            }
+
+            expectedVector.push_back(x);
+        }
+        lists.push_back(head);
+    }
+
+    ListNode* result = s.mergeKLists(lists);
+
+    sort(expectedVector.begin(), expectedVector.end()); // <algorithm>, std::sort()
+
+    ListNode *expected = NULL, *h = NULL;
+    const int n = expectedVector.size();
+    for(int i = 0; i < n; i++){
+        if(!expected){
+            expected = new ListNode(expectedVector[i]);
+            h = expected;
+        }else{
+            h->next = new ListNode(expectedVector[i]);
+            h = h->next;
+        }
+    }
+
+    assertList(expected, result);
+}
--- leetcode/src/mergeKSortedList.cpp
+++ leetcode/src/mergeKSortedList.cpp
@@ -5,47 +5,63 @@
 
 using namespace std;
 
-ListNode* merge(ListNode *l1, ListNode *l2, ListNode* &tail){
-    ListNode *h=0, *head=0;
-    if(!l1)    return l2;
-    if(!l2)    return l1;
-    if(l1->val < l2->val){
-        h = head = l1;
-        l1 = l1->next;
-    }else{
-        h = head = l2;
-        l2 = l2->next;
+class Solution{
+public:
+    /*
+     *
+     * */
+    ListNode* mergeKLists(vector<ListNode*>& lists){
+        if(lists.empty())    return NULL;
+
+        const int n = lists.size();
+        int i = 0;
+        while(i < n && lists[i] == NULL){
+            i++;
+        }
+
+        if(i == n)    return NULL;
+
+        ListNode *l1 = lists[i++];
+        for(; i < n; i++){
+            ListNode *l2 = lists[i];
+            l1 = merge(l1, l2);
+        }
+
+        return l1;
     }
-    while(l1 || l2){
-        if(!l2 || (l1 && l1->val < l2->val)){
-            h->next = l1;
+
+private:
+    /*
+     * l1 and l2 are heads of two sorted SLL
+     * */
+    ListNode* merge(ListNode *l1, ListNode *l2){
+        ListNode *h = NULL, *head = NULL;
+
+        if(!l1)    return l2;
+        if(!l2)    return l1;
+
+        if(l1->val < l2->val){
+            h = head = l1;
             l1 = l1->next;
         }else{
-            h->next = l2;
+            h = head = l2;
             l2 = l2->next;
         }
-        h = h->next;
-    }
-    tail = h;
-    return head;
-}
 
-ListNode* mergeKLists(vector<ListNode*>& lists){
-    int n = lists.size();
-    if(n<1)    return NULL;
-    int i=0;
-    while(i<n && lists[i]==NULL){
-        i++;
-    }
-    if(i==n)    return NULL;
-    ListNode *l1 = lists[i++];
-    for(;i<n;i++){
-        ListNode *l2 = lists[i], *tail = 0;
-        l1 = merge(l1, l2, tail);
-    }
-    return l1;
-}
+        while(l1 || l2){
+            if(!l2 || (l1 && l1->val < l2->val)){
+                h->next = l1;
+                l1 = l1->next;
+            }else{
+                h->next = l2;
+                l2 = l2->next;
+            }
 
-int main(int, char**){
-    return 0;
-}
+            h = h->next;
+        }
+
+        return head;
+    }
+};
+
+/* unit test is in ../cpp_unittest/mergeKSortedList_unittest */
