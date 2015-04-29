44fd82653b97d1af2cbcdec3e24d8b81dd61cb1d
teaspring <bruce.letter@gmail.com>
2015-04-29 09:58:16 +0800

    refactor mergeSortedArray.cpp, add unit test

---
+++ leetcode/cpp_unittest/mergeSortedArray_unittest/mergeSortedArray_unittest.cpp
@@ -0,0 +1,57 @@
+#include "mergeSortedArray.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * void merge(int A[], int m, int B[], int n)
+ * */
+
+void assertArray(int expected[], int result[], const int n){
+    for(int i = 0; i < n; i++){
+        EXPECT_EQ(expected[i], result[i]);
+    }
+}
+
+TEST(mergeSortedArrayTest, Positive01){
+    Solution s;
+
+    int A[10] = {1, 2, 4};
+    const int n1 = 3;
+    int B[] = {0, 3, 6, 8, 10};
+    const int n2 = sizeof(B)/sizeof(int);
+
+    s.merge(A, n1, B, n2);
+
+    int expected[] = {0, 1, 2, 3, 4, 6, 8, 10};
+
+    assertArray(expected, A, n1 + n2);
+}
+
+TEST(mergeSortedArrayTest, Positive02){
+    Solution s;
+
+    int A[10] = {6, 8, 10};
+    const int n1 = 3;
+    int B[] = {0, 1, 2, 3, 4};
+    const int n2 = sizeof(B)/sizeof(int);
+
+    s.merge(A, n1, B, n2);
+
+    int expected[] = {0, 1, 2, 3, 4, 6, 8, 10};
+
+    assertArray(expected, A, n1 + n2);
+}
+
+TEST(mergeSortedArrayTest, Positive03){
+    Solution s;
+
+    int A[10] = {1, 4, 10};
+    const int n1 = 3;
+    int B[] = {0, 2, 3, 6, 8};
+    const int n2 = sizeof(B)/sizeof(int);
+
+    s.merge(A, n1, B, n2);
+
+    int expected[] = {0, 1, 2, 3, 4, 6, 8, 10};
+
+    assertArray(expected, A, n1 + n2);
+}
--- leetcode/src/mergeSortedArray.cpp
+++ leetcode/src/mergeSortedArray.cpp
@@ -6,72 +6,56 @@
  * 3,4,5 | 1, 2
  * 1,2,2,5 | 0,2,3
  * */
-#include "../include/preliminary.h"
+#include <cstring>
 
-void swap(int *pa, int *pb){
-    int tmp = *pb;
-    *pb = *pa;
-    *pa = tmp;
-}
+using namespace std;
 
-void sortHead(int A[], int n){
-    if(n < 2)    return;
-    for(int *p = A+1; p < A+n; ++p){
-        if(*(p-1) > *p){
-            swap(p-1, p);
+class Solution{
+
+public:
+    /*
+     * in array, [0, m) and [m, m+n) are sorted separately
+     * */
+    void merge(int A[], int m, int B[], int n){
+        if(n == 0)    return;
+
+        for(int i = 0; i < n; ++i){
+            A[m+i] = B[i];
+        }
+
+        if(m == 0)    return;
+
+        int *p = A, *q = A + m;
+        while(p < q && q < A+m+n){
+            if(*p > *q){
+                swap(p, q);
+                sortHead(q, A+m+n - q);
+                continue;
+            }
+            ++p;
         }
     }
-    return;
-}
 
-/*
- * point of solution is to sort the swapped *q to make it in order with following values 
- * */
-void merge(int A[], int m, int B[], int n){
-    if(n==0)   return;
-    for(int i=0; i<n; ++i){
-        A[m+i] = B[i];
+private:
+    void swap(int *pa, int *pb){
+        int tmp = *pb;
+        *pb = *pa;
+        *pa = tmp;
     }
-    if(m==0)    return;
-    int *p = A, *q = A + m;
-    while(p < q && q < A+m+n){
-        if(*p > *q){
-            swap(p, q);
-            sortHead(q, A+m+n - q);
-            continue;
+
+    /*
+     * A[] is sorted in order initially, replace a new int to A[0], it keeps it sorted in order
+     * and A[0] remains minium of A[]
+     * */
+    void sortHead(int A[], int n){
+        if(n < 2)    return;
+
+        for(int *p = A+1; p < A+n; ++p){
+            if(*(p-1) > *p){
+                swap(p-1, p);
+            }
         }
-        ++p;
     }
-    return;
-}
+};
 
-void test(){
-    string str;
-    while(1){
-        cout << "please input sorted array B[]:" << endl;
-        if(getline(cin, str)==0 || str.empty())    break;
-        int *B = new int[str.size()]();
-        int n = splitStr2IntArray(str, B);
-        
-        cout << "please input sorted array A[]:" << endl;
-        if(getline(cin, str)==0 || str.empty())    break;
-        int *A = new int[str.size() + n]();
-        int m = splitStr2IntArray(str, A);
-        
-        merge(A, m, B, n);
-        for(int i=0; i<m+n; ++i){
-            cout << A[i] << ", ";
-        }
-        cout << endl;
-
-        delete A;
-        A = 0;
-        delete B;
-        B = 0;
-    }
-}
-
-int main(){
-    test();
-    return 0;
-}
+/* unit test is in ../cpp_unittest/mergeSortedArray_unittest */
