496af99acdd8bd96aeaac7f357e0b55571cd9bbc
teaspring <bruce.letter@gmail.com>
2015-05-04 17:37:31 +0800

    remove unused code

--- leetcode/cpp_unittest/maxPoints_unittest/maxPoints_unittest.cpp
+++ leetcode/cpp_unittest/maxPoints_unittest/maxPoints_unittest.cpp
@@ -2,11 +2,10 @@
 #include "gtest/gtest.h"
 
 /*
- * int maxPoints_01(vector<Point>&)  // may has bug
- * int maxPoints_02(vector<Point>&)
+ * int maxPoints(vector<Point>&)
  * */
 
-TEST(maxPoints01_Test, Positive01){
+TEST(maxPoints_Test, Positive01){
     Solution s;
 
     int arr[] = {1,1, 3,3, 0,-1, 1,0, 4,4, 3,4, 5,4, 2,2, 1,2, 0,0, 2,1, 3,3};
@@ -19,30 +18,12 @@ TEST(maxPoints01_Test, Positive01){
 
     int expected = 6;
 
-    EXPECT_EQ(expected, s.maxPoints_01(points));
+    EXPECT_EQ(expected, s.maxPoints(points));
 
     points.clear();
 }
 
-TEST(maxPoints02_Test, Positive01){
-    Solution s;
-
-    int arr[] = {1,1, 3,3, 0,-1, 1,0, 4,4, 3,4, 5,4, 2,2, 1,2, 0,0, 2,1, 3,3};
-
-    const int n = sizeof(arr) / sizeof(int);
-    vector<Point> points;
-    for(int i = 0; i < (n >> 1); i++){
-        points.push_back(Point(arr[2*i], arr[2*i + 1]));
-    }
-
-    int expected = 6;
-
-    EXPECT_EQ(expected, s.maxPoints_02(points));
-
-    points.clear();
-}
-
-TEST(maxPoints02_Test, Positive02){
+TEST(maxPoints_Test, Positive02){
     Solution s;
 
     int arr[] = {1,1, 3,3, 0,-1, 4,4};
@@ -55,7 +36,7 @@ TEST(maxPoints02_Test, Positive02){
 
     int expected = 3;
 
-    EXPECT_EQ(expected, s.maxPoints_02(points));
+    EXPECT_EQ(expected, s.maxPoints(points));
 
     points.clear();
 }
--- leetcode/src/maxPoints.cpp
+++ leetcode/src/maxPoints.cpp
@@ -6,8 +6,8 @@
  * 2. if there is duplicate points
  * 3. all points are duplicate
  * */
+
 #include <vector>
-#include <cstring>
 #include <map>
 #include <algorithm>
 
@@ -19,70 +19,14 @@ struct Point{
     Point(int a, int b) : x(a), y(b){}
 };
 
-struct Line{
-    int start;
-    int end;
-    Line(int s = 0, int e = 0) : start(s), end(e){}
-};
-
 class Solution{
 
 public:
     /*
-     *
+     * fix a line with one point and slope, OK, how to present slope ? as it is not integer and float has decision dependency
+     * the idea with two integer x and y pair to present slope is very good, elegent
      * */
-    int maxPoints_01(vector<Point> &points){
-        sort(points.begin(), points.end(), point_cmp);  // sorted
-
-        const int n = points.size();
-        if(n <= 2)    return n;
-
-        vector<Point> upoints;  // unique points without duplicate
-        upoints.push_back(points[0]);
-
-        int dups[n];  // duplicate count of unique point
-        dups[0] = 1;
-
-        int m = 1;    // m is count of all unique points
-
-        for(int i = 1; i < n; i++){
-            if(points[i].x == points[i-1].x
-            && points[i].y == points[i-1].y){
-                dups[m-1]++;
-            }else{
-                m++;
-                dups[m-1] = 1;
-                upoints.push_back(points[i]);
-            }
-        }
-
-        if(m == 1)    return n;  //only 1 unique point, unnecessary to get slope any more!
-
-        map<float, vector<Line> > slopes;
-
-        for(int i = 0; i < m-1; ++i){    // calculate slope within unique points
-            for(int j = i+1; j < n; ++j){
-                float f = slope(upoints[i], upoints[j]);
-                if(slopes.count(f) == 0){
-                    vector<Line> vec;
-                    slopes[f] = vec;
-                }
-                slopes[f].push_back(Line(i, j));
-            }
-        }
-
-        int res = 2;
-        for(map<float, vector<Line> >::iterator iter = slopes.begin();
-            iter != slopes.end(); ++iter){
-            res = max(res, maxNodesOnLine(iter->second, dups));
-        }
-        return res;
-    }
-
-    /*
-     *
-     * */
-    int maxPoints_02(vector<Point> &points){
+    int maxPoints(vector<Point> &points){
         const int n = points.size();
         if(n < 2)    return n;
 
@@ -130,39 +74,6 @@ public:
     }
 
 private:
-    // used by maxPoints_01()
-    float slope(Point &p1, Point &p2){ // slope of two unique points, take care of p1.x == p2.x
-        return p1.x == p2.x ? 0xffff : (float)(p1.y - p2.y) / (float)(p1.x - p2.x);
-    }
-
-    // used by maxPoints_01()
-    int maxNodesOnLine(vector<Line> &lines, int dups[]){
-        const int n = lines.size();
-        int res = 2, curr = 2;
-
-        for(int i = 1; i <= n; ++i){  // implicit tip: process the edge i==n !
-            if(i == n || lines[i].start != lines[i-1].start){
-                curr += dups[lines[i-1].start] - 1;  // complement the duplicate points
-                res = max(res, curr);
-                curr = 2;
-            }else{
-                curr++;
-            }
-        }
-        return res;
-    }
-
-    static bool point_cmp(Point p1, Point p2){
-        if(p1.x < p2.x){
-            return true;
-        }else if(p1.x > p2.x){
-            return false;
-        }else{
-            return p1.y <= p2.y;
-        }
-    }
-
-    // used by maxPoints_02()
     int getGCD(int a, int b){
         return b == 0 ? a : getGCD(b, a % b);
     }
