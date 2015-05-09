c1cfb20ddde7d26c9cf635135db0a37949c9244e
teaspring <bruce.letter@gmail.com>
2015-05-09 13:31:26 +0800

    refactor solveSudoku.cpp, add unit test

---
+++ leetcode/cpp_unittest/solveSudoku_unittest/solveSudoku_unittest.cpp
@@ -0,0 +1,60 @@
+#include "solveSudoku.cpp"
+#include "gtest/gtest.h"
+
+/*
+ * int Solution::solveSudoku(vector<int>&)
+ * */
+
+void assertBoard(vector<vector<char> >& expected, vector<vector<char> >& result){
+    EXPECT_EQ(expected.size(), result.size());
+    const int rows = expected.size();
+    const int cols = expected[0].size();
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            EXPECT_EQ(expected[i][j], result[i][j]);
+        }
+    }
+}
+
+/* ---------------- test body ---------------- */
+TEST(solveSudokuTest, Positive01){
+    Solution s;
+
+    const int rows = 9, cols = 9;
+
+    char A[][cols] = {
+        {'7', '.', '.', '.', '.', '.', '.', '.', '9'},
+        {'.', '5', '1', '.', '.', '.', '6', '7', '.'},
+        {'.', '6', '.', '.', '2', '.', '.', '4', '.'},
+        {'3', '9', '.', '2', '.', '5', '.', '8', '7'},
+        {'.', '.', '4', '.', '.', '.', '2', '.', '.'},
+        {'2', '8', '.', '3', '.', '1', '.', '9', '4'},
+        {'.', '2', '.', '.', '1', '.', '.', '5', '.'},
+        {'.', '7', '8', '.', '.', '.', '9', '6', '.'},
+        {'4', '.', '.', '.', '.', '.', '.', '.', '1'}};
+
+    vector<vector<char> > result;
+    for(int i = 0; i < rows; i++){
+        result.push_back(vector<char>(A[i], A[i] + cols));
+    }
+
+    s.solveSudoku(result);
+
+    char B[][cols] = {
+        {'7', '4', '2', '5', '8', '6', '3', '1', '9'},
+        {'8', '5', '1', '9', '3', '4', '6', '7', '2'},
+        {'9', '6', '3', '1', '2', '7', '8', '4', '5'},
+        {'3', '9', '6', '2', '4', '5', '1', '8', '7'},
+        {'5', '1', '4', '8', '7', '9', '2', '3', '6'},
+        {'2', '8', '7', '3', '6', '1', '5', '9', '4'},
+        {'6', '2', '9', '7', '1', '3', '4', '5', '8'},
+        {'1', '7', '8', '4', '5', '2', '9', '6', '3'},
+        {'4', '3', '5', '6', '9', '8', '7', '2', '1'}};
+
+    vector<vector<char> > expected;
+    for(int i = 0; i < rows; i++){
+        expected.push_back(vector<char>(B[i], B[i] + cols));
+    }
+
+    assertBoard(expected, result);
+}
--- leetcode/src/solveSudoku.cpp
+++ leetcode/src/solveSudoku.cpp
@@ -1,50 +1,67 @@
 /*
  * solve a sudoku of 9*9 board. null cell is marked as '.'
  * */
-#include "stdio.h"
-#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 class Solution{
+
 public:
+    /*
+     * verify each alternative int 0-9 on every cell position recursely
+     * */
     void solveSudoku(vector<vector<char> >& board){
         dfs(board, 0);
     }
 
+private:
     bool dfs(vector<vector<char> >& board, int pos){
-        int n = board.size();
+        const int n = board.size();
+
         if(pos == n*n)    return true;
-        int x = pos / n, y = pos % n;
+
+        int x = pos / n, y = pos % n; // x is n-1 in maximum, y is n-1 in maximum
+
         if(board[x][y] == '.'){
             for(char ch = '1'; ch <= '9'; ch++){
                 board[x][y] = ch;
-                if(validate(board, pos) && dfs(board, pos+1))    return true;
+                if(validate(board, pos) && dfs(board, pos + 1))    return true;
             }
+
             board[x][y] = '.';
         }else{
-            if(dfs(board, pos+1))    return true;
+            if(dfs(board, pos + 1))    return true;
         }
+
         return false;
     }
 
+    /*
+     * validate row, column, and the 3*3 cubic that this point resides
+     * */
     bool validate(vector<vector<char> >& board, int pos){
-        int n = board.size();
-        int x = pos/n, y = pos%n;
+        const int n = board.size();
+
+        int x = pos / n, y = pos % n; // both x and y are n-1 in maximum
+
         char ch = board[x][y];
-        for(int i=0; i<n; i++){
+
+        for(int i = 0; i < n; i++){  // that row and column that [x][y] resides
             if(i != x && board[i][y] == ch)    return false;
             if(i != y && board[x][i] == ch)    return false;
         }
-        int cx = x/3 * 3;   // 3 is the height and width of the 3*3 cubic of sudoku
+
+        int cx = x/3 * 3;  // 3 is the height and width of the 3*3 cubic of sudoku
         int cy = y/3 * 3;
-        for(int i=cx; i<cx+3; i++){
-            for(int j=cy; j<cy+3; j++){
+
+        for(int i = cx; i < cx + 3; i++){
+            for(int j = cy; j < cy + 3; j++){
                 if(i != x && j != y && board[i][j] == ch)    return false;
             }
         }
+
         return true;
     }
 };
