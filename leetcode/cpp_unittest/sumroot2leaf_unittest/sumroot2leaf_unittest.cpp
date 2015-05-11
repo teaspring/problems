6643421eeaa7928a645637155e4bea92c1724b07
teaspring <bruce.letter@gmail.com>
2015-05-11 10:43:37 +0800

    refactor sumroot2leaf.cpp

--- leetcode/cpp_unittest/sumroot2leaf_unittest/sumroot2leaf_unittest.cpp
+++ leetcode/cpp_unittest/sumroot2leaf_unittest/sumroot2leaf_unittest.cpp
@@ -3,7 +3,8 @@
 
 /*
  * sumroot2leaf.cpp:
- * int sumNumbers(TreeNode* root)
+ * int sumNumbers_01(TreeNode* root)
+ * int sumNumbers_02(TreeNode* root)
  *
  * preliminary.cpp:
  * struct TreeNode
@@ -11,19 +12,19 @@
  * */
 
 /* ------------- test body -------------- */
-TEST(sumroot2leaf_Test, Positive01){
+TEST(sumroot2leaf01_Test, Positive01){
     Solution s;
 
     TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
 
-    EXPECT_EQ(12 + 13, s.sumNumbers(root));
+    EXPECT_EQ(12 + 13, s.sumNumbers_01(root));
 
     delTree(root);
 }
 
-TEST(sumroot2leaf_Test, Positive02){
+TEST(sumroot2leaf01_Test, Positive02){
     Solution s;
 
     TreeNode *root = new TreeNode(1);
@@ -34,7 +35,77 @@ TEST(sumroot2leaf_Test, Positive02){
     root->right->left = new TreeNode(4);
     root->right->right = new TreeNode(5);
 
-    EXPECT_EQ(121 + 120 + 134 + 135, s.sumNumbers(root));
+    EXPECT_EQ(121 + 120 + 134 + 135, s.sumNumbers_01(root));
+
+    delTree(root);
+}
+
+TEST(sumroot2leaf01_Test, Positive03){
+    Solution s;
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    root->left->left->right = new TreeNode(6);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->left->right = new TreeNode(8);
+    root->right->left->right = new TreeNode(8);
+    root->right->right->right = new TreeNode(9);
+
+    EXPECT_EQ(1246 + 12578 + 1368 + 1379, s.sumNumbers_01(root));
+
+    delTree(root);
+}
+
+TEST(sumroot2leaf02_Test, Positive01){
+    Solution s;
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    EXPECT_EQ(12 + 13, s.sumNumbers_02(root));
+
+    delTree(root);
+}
+
+TEST(sumroot2leaf02_Test, Positive02){
+    Solution s;
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(0);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(5);
+
+    EXPECT_EQ(121 + 120 + 134 + 135, s.sumNumbers_02(root));
+
+    delTree(root);
+}
+
+TEST(sumroot2leaf02_Test, Positive03){
+    Solution s;
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    root->left->left->right = new TreeNode(6);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->left->right = new TreeNode(8);
+    root->right->left->right = new TreeNode(8);
+    root->right->right->right = new TreeNode(9);
+
+    EXPECT_EQ(1246 + 12578 + 1368 + 1379, s.sumNumbers_02(root));
 
     delTree(root);
 }
--- leetcode/src/sumroot2leaf.cpp
+++ leetcode/src/sumroot2leaf.cpp
@@ -1,6 +1,8 @@
 /*
  * given a binary tree containing digits from 0-9. each root-to-leaf could represent a number.
  * an example is 1->2->3 which represents the number 123.
+ *
+ * both BFS and DFS can solve this problem
  * */
 
 #include "../include/preliminary.h"
@@ -9,39 +11,84 @@ class Solution{
 
 public:
     /*
-     *
+     * BFS, with one more stack<> for traverse
      * */
-    int sumNumbers(TreeNode *root){
+    int sumNumbers_01(TreeNode *root){
         if(!root)    return 0;
 
-        stack<TreeNode*> stk;
+        stack<TreeNode*> stk; // used for BFS traverse
         stk.push(root);
 
         int res = 0;
 
-        stack<TreeNode*> snums;
+        vector<TreeNode*> snums; // path for root to leaf
 
         while(!stk.empty()){
             TreeNode *p = stk.top();
             stk.pop();
 
+            // pop elements rom snums to keep its tail remains parent of p
             while(!snums.empty()
-                && snums.top()->left != p
-                && snums.top()->right != p){
+                && snums[snums.size() - 1]->left != p
+                && snums[snums.size() - 1]->right != p){
 
-                snums.pop();
+                snums.pop_back();
             }
 
-            snums.push(p);
+            snums.push_back(p);
 
-            if(!(p->left) && !(p->right)){  // reach leaf
-                res += sumStackAsNumber(snums);
-                snums.pop();
+            if(!(p->left) && !(p->right)){  // p is leaf
+                res += sumList(snums);
+                snums.pop_back();
             }
 
+            // push right firstly then pop left first
             if(NULL != p->right)    stk.push(p->right);
-
             if(NULL != p->left)     stk.push(p->left);
+        }
+
+        return res;
+    }
+
+    /*
+     * DFS, its logic is more complex than BFS
+     * */
+    int sumNumbers_02(TreeNode *root){
+        if(!root)    return 0;
+
+        int res = 0;
+
+        vector<TreeNode*> path;
+        path.push_back(root);
+
+        TreeNode *p = root, *last = NULL;
+
+        while(!path.empty()){
+            p = path[path.size() - 1];
+
+            if(last == p->right){  // left and right child of p have been traversed, skip p
+                last = p;
+                path.pop_back();
+                continue;
+            }
+
+            if(last != p->left){
+                while(NULL != p->left){ // traverse along the left child
+                    p = p->left;
+                    path.push_back(p);
+                }
+            }
+
+            if(!(p->left) && !(p->right)){  // p is leaf, sum current path
+                res += sumList(path);
+                path.pop_back();
+            }else if(NULL != p->right){  // right sub tree of p has not been traversed yet, push it
+                path.push_back(p->right);
+            }else{  // p has no right child, skip p
+                path.pop_back();
+            }
+
+            last = p;
         }
 
         return res;
@@ -49,25 +96,14 @@ public:
 
 private:
     /*
-     *
+     * present the vector 1->2->3 as integer 123
      * */
-    int sumStackAsNumber(stack<TreeNode*>& stk){
-
+    int sumList(const vector<TreeNode*>& path){
         int sum = 0;
-        stack<TreeNode*> stk2;
-        int d = 1;
+        const int n = path.size();
 
-        while(!stk.empty()){
-            stk2.push(stk.top());
-            sum += d * stk.top()->val;
-            stk.pop();
-
-            d *= 10;
-        }
-
-        while(!stk2.empty()){
-            stk.push(stk2.top());
-            stk2.pop();
+        for(int i = 0; i < n; i++){
+            sum  = sum * 10 + path[i]->val;
         }
 
         return sum;
