9be5c7274376dc999b5dc6b81abe1270c0d69b79
teaspring <bruce.yuzh@gmail.com>
2016-11-29 16:46:49 +0800

    maxstockprofit problemIV, initial version but with a bug

--- leetcode/cpp_unittest/maxstockprofit_unittest/maxstockprofit_unittest.cpp
+++ leetcode/cpp_unittest/maxstockprofit_unittest/maxstockprofit_unittest.cpp
@@ -1,16 +1,93 @@
 #include "maxstockprofit.cpp"
 #include "gtest/gtest.h"
 
-/* int Solution::maxProfitIII(const vector<int>& prices) */
-
-TEST(maxstockprofitTest, Positive01){
+/*----- int maxProfitIII(const vector<int>&) -----*/
+TEST(maxstockprofitIIITest, Positive01){
     Solution s;
 
     int arr[] = {1, 2, 4, 3, 1, 2, 3, 2, 5};
-    vector<int> prices(arr, arr + sizeof(arr) / sizeof(int));
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
 
-    int expected = 7;
+    int expected = 7; //[buy,0,sell,0,buy,0,0,0,sell]
     EXPECT_EQ(expected, s.maxProfitIII(prices));
+
+    prices.clear();
+}
+
+/*----- int getAllAvailableProfit(const vector<int>&, int&) -----*/
+TEST(getAllAvailableProfitTest, Positive01) {
+    Solution s;
+
+    int arr[] = {1, 2, 4, 3, 1, 2, 3, 2, 5};
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
+
+    int expectedCount = 3;
+    int expectedSum = 8; // [buy,0,sell,0,buy,0,sell,buy,sell]
+
+    int cnt = 0;
+    int sum = s.getAllAvailableProfit(prices, cnt);
+    EXPECT_EQ(expectedCount, cnt);
+    EXPECT_EQ(expectedSum, sum);
+
+    prices.clear();
+}
+
+TEST(getAllAvailableProfitTest, Positive02) {
+    Solution s;
+
+    int arr[] = {3, 2, 2, 1, 1, 2, 4, 4, 3, 1, 2, 2, 3, 3, 2, 5};
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
+
+    int expectedCount = 3;
+    int expectedSum = 8;
+
+    int cnt = 0;
+    int sum = s.getAllAvailableProfit(prices, cnt);
+    EXPECT_EQ(expectedCount, cnt);
+    EXPECT_EQ(expectedSum, sum);
+
+    prices.clear();
+}
+
+/*----- int maxProfitIV(int, const vector<int>&) -----*/
+TEST(maxstockprofitIVTest, Positive01) {
+    Solution s;
+
+    int arr[] = {3, 2, 2, 1, 1, 2, 4, 4, 3, 1, 2, 2, 3, 3, 2, 5};
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
+
+    int k = 3;
+    int expected = 8; // [buy,0,sell,0,buy,0,sell,buy,sell]
+    EXPECT_EQ(expected, s.maxProfitIV(k, prices));
+
+    k = 4;
+    EXPECT_EQ(expected, s.maxProfitIV(k, prices));
+
+    prices.clear();
+}
+
+TEST(maxstockprofitIVTest, Positive02) {
+    Solution s;
+
+    int arr[] = {3, 2, 1, 2, 2, 4, 3, 1, 2, 2, 3, 3, 2, 1, 2, 4, 2, 5};
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
+
+    int k = 2;
+    int expected = 7;
+    EXPECT_EQ(expected, s.maxProfitIV(k, prices));
+
+    prices.clear();
+}
+
+TEST(maxstockprofitIVTest, Positive03) {
+    Solution s;
+
+    int arr[] = {3, 2, 1, 2, 2, 4, 3, 1, 2, 2, 3, 3, 2, 1, 2, 4, 2, 5};
+    vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
+
+    int k = 3;
+    int expected = 9;
+    EXPECT_EQ(expected, s.maxProfitIV(k, prices));
 
     prices.clear();
 }
--- leetcode/src/maxstockprofit.cpp
+++ leetcode/src/maxstockprofit.cpp
@@ -1,8 +1,8 @@
 /*
  * Best Time to Buy and Sell Stock
- * III.Given an array for which the ith element is the price of a given stock on day i. Find the maximum profit with at most two transactions
- * test data: 
- * gas{1,2,3,4,5}, cost{3,4,5,1,2}
+ * Problem III: Given an array for which the ith element is the price of a given stock on day i. Find the maximum profit with at most two transactions
+ *
+ * Problem IV: find the maximum profit with at most k transactions
  * */
 #include "../include/preliminary.h"
 
@@ -17,24 +17,95 @@ public:
 
         int history[n];
         memset(history, 0, sizeof(history));
+
+        int low = prices[0];
+        for(int i=1; i < n; ++i){
+            low = min(low, prices[i]);
+            history[i] = max(history[i-1], prices[i] - low); // max profit for days [0, i] with 1 transaction, day i can sell out
+        }
+
         int future[n];
         memset(future, 0, sizeof(future));
 
-        int valley = prices[0];
-        for(int i = 1;i < n; ++i){
-            valley = min(valley, prices[i]);
-
-            history[i] = max(history[i-1], prices[i] - valley); // max profit for days [0, i]
-        }
-
-        int peak = prices[n-1], sum = 0;
-        for(int i = n-2;i >= 0; --i){
-            peak = max(peak, prices[i]);
-            future[i] = max(future[i+1], peak - prices[i]); // max profit for days [i, n-1]
+        int high = prices[n-1], sum = 0;
+        for(int i = n-2; i >= 0; --i){
+            high = max(high, prices[i]);
+            future[i] = max(future[i+1], high - prices[i]); // max profit for days [i, n-1] with 1 transaction, day i can buy in
 
             sum = max(sum, history[i] + future[i]);
         }
 
+        return sum;
+    }
+
+    /**
+     * idea comes from solution to maxProfitIII. total time in O(n^3)...
+     * */
+    int maxProfitIV(int k, const vector<int>& prices) {
+        int cnt = 0;
+        int sum = getAllAvailableProfit(prices, cnt);
+        if (k >= cnt) {
+            return sum;
+        }
+
+        const int n = prices.size();
+        int dp[k+1][n+1]; // dp[i][j] means max profit with i transactions mostly among prices[1...n]
+        for(int i=0; i < k; ++i) {
+            memset(dp[i], 0, sizeof(dp[i])); // note the meaning of sizeof(int[])
+        }
+
+        int low = prices[0];
+        for(int i=1; i < n; ++i) {  // init dp[1][]
+            low = min(low, prices[i]);
+            dp[1][i+1] = max(dp[1][i], prices[i] - low);
+        }
+
+        int future[n];
+        memset(future, 0, sizeof(future));
+
+        int high = prices[n-1];
+        for(int i = n-2; i >= 0; --i){
+            high = max(high, prices[i]);
+            future[i] = max(future[i+1], high - prices[i]); // max profit for days [i, n-1] with 1 transaction, day i can buy in
+        }
+
+        for(int i=2; i <= k; ++i) {
+            for(int j=1; j <= n; ++j) {
+                int sum = 0;
+                for(int k = j-2; k >= 0; --k) {
+                    sum = max(sum, dp[i-1][k] + future[k]);
+                }
+                dp[i][j] = sum;
+            }
+        }
+        return dp[k][n];
+    }
+
+    /**
+     * get maximum potential profit if all the available transactions are executed
+     * */
+    int getAllAvailableProfit(const vector<int>& prices, int& count) {
+        count = 0;
+        const int n = prices.size();
+        if(n < 2)    return 0;
+
+        int sum = 0;
+        int lowIdx = -1, low = 0, high = 0;
+        for(int i=1; i <= n; i++) {
+            if (i == n || prices[i] < prices[i-1]) {  // decending or reach end
+                if (lowIdx != -1) {
+                    count++;
+                    sum += high - low;
+                    lowIdx = -1;
+                }
+            } else if(prices[i] > prices[i-1]){  // ascending
+                if(lowIdx == -1) {
+                    lowIdx = i-1;
+                    low = prices[lowIdx];
+                }
+                high = prices[i];
+            }
+        }
         return sum;
     }
 };
