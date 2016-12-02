250883782fc16d3a8712b912b6852dfb31b96344
teaspring <bruce.yuzh@gmail.com>
2016-12-02 15:15:52 +0800

    robHouse Promelm II

--- leetcode/cpp_unittest/robHouse_unittest/robHouse_unittest.cpp
+++ leetcode/cpp_unittest/robHouse_unittest/robHouse_unittest.cpp
@@ -1,19 +1,92 @@
 #include "robHouse.cpp"
 #include "gtest/gtest.h"
 
-/*
- * int Solution::rob(vector<int>&)
- * */
-TEST(robHouseI_Test, Positive1_01){
+/* int Solution::rob(vector<int>&) */
+TEST(robHouseI_Test, Positive1){
     Solution s;
 
     int arr[] = {3, 4, 2, 3, 1, 2, 3};
     const int n = sizeof(arr) / sizeof(int);
     vector<int> nums(arr, arr + n);
 
-    int expected = 10;
-
-    EXPECT_EQ(expected, s.rob(nums));
+    EXPECT_EQ(10, s.rob(nums));
     nums.clear();
 }
 
+TEST(robHouseI_Test, Positive2){
+    Solution s;
+
+    int arr[] = {1, 3, 2, 1, 1, 3, 4, 2, 2};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(10, s.rob(nums));
+    nums.clear();
+}
+
+TEST(robHouseI_Test, Positive3){
+    Solution s;
+
+    int arr[] = {1, 3, 1, 2, 5};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(8, s.rob(nums));
+    nums.clear();
+}
+
+TEST(robHouseI_Test, Positive4){
+    Solution s;
+
+    int arr[] = {1, 1, 10};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(11, s.rob(nums));
+    nums.clear();
+}
+
+TEST(robHouseI_Test, Positive5){
+    Solution s;
+
+    int arr[] = {1, 1, 10, 1};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(11, s.rob(nums));
+    nums.clear();
+}
+
+TEST(robHouseI_Test, Edge1){
+    Solution s;
+
+    int arr[] = {1};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(1, s.rob(nums));
+    nums.clear();
+}
+
+/* int Solution::robII(vector<int>&) */
+TEST(robHouseII_Test, Positive1){
+    Solution s;
+
+    int arr[] = {1, 1, 10};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(10, s.robII(nums));
+    nums.clear();
+}
+
+TEST(robHouseII_Test, Positive2){
+    Solution s;
+
+    int arr[] = {1, 1, 10, 1};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    EXPECT_EQ(11, s.robII(nums));
+    nums.clear();
+}
--- leetcode/src/robHouse.cpp
+++ leetcode/src/robHouse.cpp
@@ -1,6 +1,8 @@
 /*
  * Problem I: given a list of non-negative integers representing the amount of money of each house. determine the max amount of money you
  * can rob tonight without alerting the police via robbing two adjacent houses.
+ *
+ * Problem II: extension of Problem I, all houses are placed in circle.
  * */
 
 #include <vector>
@@ -13,25 +15,47 @@ using namespace std;
 class Solution{
 public:
     int rob(const vector<int>& nums) {
+        return robFromTo_02(nums, 0, nums.size());
+    }
+
+    int robII(const vector<int>& nums) {
         const int n = nums.size();
         if(n == 0)    return 0;
         if(n == 1)    return nums[0];
-        if(n == 2)    return max(nums[0], nums[1]);
+        return max(robFromTo_02(nums, 0, n-1), robFromTo_02(nums, 1, n)); // smart process of the two options
+    }
+
+private:
+    /*
+     * start is inclusive, end is exclusive
+     * */
+    int robFromTo_01(const vector<int>& nums, int start, int end) {
+        const int n = nums.size();
+        if(n == 0 || start < 0 || start >= n || end <= start || end > n)
+          return 0;
 
         int robbery[n];
         memset(robbery, 0, sizeof(robbery));
-        robbery[0] = nums[0];
-        robbery[1] = nums[1];
+        robbery[start] = nums[start];
 
-        int maxRob[n];
-        memset(maxRob, 0, sizeof(maxRob));
-        maxRob[0] = robbery[0];
-        maxRob[1] = max(robbery[0], robbery[1]);
-
-        for(int i = 2; i < n; ++i) {
-            robbery[i] = maxRob[i-2] + nums[i];
-            maxRob[i] = max(robbery[i-1], robbery[i]);
+        for(int i = start; i < end; ++i) {
+            int tmp = (i - start > 1 ? robbery[i-2] : 0) + nums[i];
+            robbery[i] = max((i > start ? robbery[i-1] : 0), tmp);
         }
-        return maxRob[n-1];
+        return robbery[end-1];
+    }
+
+    /*
+     * more simple and more concise
+     * it can be extracted from _01 by using two int var instead of int[]
+     * */
+    int robFromTo_02(const vector<int>& nums, int start, int end) {
+        int include = 0, exclude = 0; // exclude means max robbery without nums[i]
+        for(int i = start; i < end; i++) {
+            int inc = include;
+            include = exclude + nums[i];
+            exclude = max(exclude, inc);
+        }
+        return max(include, exclude);
     }
 };
