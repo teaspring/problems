607b006d6a8d71baedef377f3cd6487cb2eb0a6b
teaspring <bruce.yuzh@gmail.com>
2017-02-03 14:54:41 +0800

    minSubArrayLen(), one more solution in time O(nlgn)

--- leetcode/cpp_unittest/minSubArrayLen_unittest/minSubArrayLen_unittest.cpp
+++ leetcode/cpp_unittest/minSubArrayLen_unittest/minSubArrayLen_unittest.cpp
@@ -1,10 +1,8 @@
 #include "minSubArrayLen.cpp"
 #include "gtest/gtest.h"
 
-/*
- * int minSubArrayLen_01(int s, vector<int>& nums)
- * */
-TEST(minSubArrayLenTest, Positive1_01){
+// int minSubArrayLen_01(int s, vector<int>& nums)
+TEST(minSubArrayLen01_Test, Positive_01){
     Solution s;
 
     int arr[] = {2, 3, 1, 2, 4, 3};
@@ -17,7 +15,7 @@ TEST(minSubArrayLenTest, Positive1_01){
     nums.clear();
 }
 
-TEST(minSubArrayLenTest, Positive1_02){
+TEST(minSubArrayLen01_Test, Positive_02){
     Solution s;
 
     int arr[] = {2, 3, 1, 2, 2, 1, 3, 6, 4, 3};
@@ -27,5 +25,49 @@ TEST(minSubArrayLenTest, Positive1_02){
 
     int expected = 1; // [6]
     EXPECT_EQ(expected, s.minSubArrayLen_01(sum, nums));
+    nums.clear();
+}
+
+// int minSubArrayLen_02(int s, vector<int>& nums)
+TEST(minSubArrayLen02_Test, Positive_01){
+    Solution s;
+
+    int arr[] = {2, 3, 1, 2, 4, 3};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+    int sum = 7;
+
+    int expected = 2; // [4,3]
+    EXPECT_EQ(expected, s.minSubArrayLen_02(sum, nums));
+    nums.clear();
+}
+
+TEST(minSubArrayLen02_Test, Positive_02){
+    Solution s;
+
+    int arr[] = {2, 3, 1, 2, 2, 1, 3, 6, 4, 3};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+    int sum = 6;
+
+    int expected = 1; // [6]
+    EXPECT_EQ(expected, s.minSubArrayLen_02(sum, nums));
+    nums.clear();
+}
+
+TEST(minSubArrayLen02_Test, Positive_03){
+    Solution s;
+
+    int arr[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+    const int n = sizeof(arr) / sizeof(int);
+    vector<int> nums(arr, arr + n);
+
+    int sum = 2;
+    int expected = 3;
+    EXPECT_EQ(expected, s.minSubArrayLen_02(sum, nums));
+
+    sum = 3;
+    expected = 5;
+    EXPECT_EQ(expected, s.minSubArrayLen_02(sum, nums));
     nums.clear();
 }
--- leetcode/src/minSubArrayLen.cpp
+++ leetcode/src/minSubArrayLen.cpp
@@ -16,9 +16,8 @@ public:
         int begin = 0, end = 0, sum = 0;
         int res = 0;
         while(begin < n && end < n) {
-            while(sum < s && end < n) {  // move end index
+            for(;sum < s && end < n; ++end) {  // move end index
                 sum += nums[end];
-                ++end;
             }
             if(sum < s)    break; // else, end == n
 
@@ -28,11 +27,49 @@ public:
             int diff = end - begin;
             res = (res == 0 ? diff : min(res, diff));
             
-            int offset = diff - res + 1;
-            for(int i = begin; i < begin + offset; ++i) {  // move begin index by offset to find the next less length
+            int offset = diff - res + 2; // +2 is better than +1
+            for(int i = begin; i < n && i < begin + offset; ++i) {  // move begin index by offset to find the next less length
                 sum -= nums[i];
             }
             begin += offset;
+        }
+        return res;
+    }
+
+    // another solution, time in O(nlgn)
+    int minSubArrayLen_02(int s, vector<int>& nums) {
+        int u = 1, v = nums.size() + 1; // [u,v)
+        while(u < v) {
+            int m = (u + v) >> 1;
+            int sum1 = maxSubArrayFixedLen(nums, m);
+            int sum2 = maxSubArrayFixedLen(nums, m-1);
+            if(sum2 < s && s <= sum1)    return m;
+            if(s <= sum2) {
+                v = m;
+            } else if (sum1 <= s) {
+                u = m+1;
+            } else {
+                u = m+1;
+            }
+        }
+        return 0;
+    }
+
+private:
+    int maxSubArrayFixedLen(vector<int>& nums, const int len) {
+        const int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < len && i < n; ++i) {
+            sum += nums[i];
+        }
+        if(len < 1)    return 0;
+        if(len >= n)    return sum;
+
+        int res = sum;
+        for(int i = len; i < n; ++i) {
+            sum -= nums[i - len];
+            sum += nums[i];
+            res = max(res, sum);
         }
         return res;
     }
